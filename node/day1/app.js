be5a4c157cbe374b2e14a5eb14a4012b814f6bd4
amandeepdhillon123 <amandeep24397@gmail.com>
2023-03-27 09:17:51 +0530

    module topic

---
+++ node/day1/app.js
@@ -0,0 +1,11 @@
+// const app = require('./index')
+
+// // console.log(app.simpleMesaage )
+
+// app.log("hello ram")
+
+var person = require('./index.js');
+
+var person1 = new person('James', 'Bond');
+
+console.log(person1.fullName());
