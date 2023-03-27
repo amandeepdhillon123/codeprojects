5d90f3a342dc8f2dcbdea1fc97ddbfaae26ec04c
amandeepdhillon123 <amandeep24397@gmail.com>
2023-03-27 09:55:10 +0530

    create file

---
+++ node/day2/Read.md
@@ -0,0 +1,27 @@
+
+ file system  
+
+  ------const fs =require('fs)
+  ---- fs.writeFIleSync("file name","file contect " )
+
+  dont not repeat 
+
+  --- const {writeFIleSync} = require('fs);
+
+  writeFIleSync("file name","file contect " )
+
+   dont not repeat 
+
+  --- const ram = require('fs).writeFIleSync;
+
+  ram("file name","file contect " )
+
+  ---- directory name 
+
+  console.log("hello director", __dirname)
+
+  --- filename
+
+  console.log("hello director",__filename)
+
+  
---
+++ node/day2/index.js
@@ -0,0 +1,25 @@
+
+
+// const fs =require('fs');
+
+// create file 
+
+// fs.writeFileSync("hello.txt"," i ama amandeep");
+// fs.writeFileSync("hello.txt2"," i ama amandeep");
+// fs.writeFileSync("hello.txt3"," i ama amandeep");
+// fs.writeFileSync("hello.txt4"," i ama amandeep");
+
+//  repeated writefilesyn c
+
+// const {writeFileSync} = require('fs');
+const fs= require('fs').writeFileSync;
+
+fs("code.txt","ram ram ji"); 
+
+
+// directory name 
+console.log(">>>>>>>",__dirname)
+
+//  file name 
+
+console.log("====",__filename)
