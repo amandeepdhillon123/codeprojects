efe2a2599261192c7731e50ea05f74183764d14b
amandeepdhillon123 <amandeep24397@gmail.com>
2023-04-03 15:45:50 +0530

    filter the value

---
+++ javacript/4.higherorder/map.js
@@ -0,0 +1,27 @@
+
+var nam=["aman","ram","shyam","love"]
+
+var result =nam.map(function(ele,index,arr){
+  return ele
+})
+
+console.log(result[0])
+
+
+
+var brand=[{brand:"samasung" ,model:256},{brand:"samasung" ,model:256}]
+
+var mobile= brand.map(function(ele) {
+    return ele
+})
+
+console.log(mobile[0])
+
+
+var num=[2,3,4,5,6]
+
+let even=num.map(function(ele) {
+    return ele%2==0
+})
+
+console.log(even)
