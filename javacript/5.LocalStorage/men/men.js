3ad572e9b6b29f04c26de8e87de0ad689fd3461b
amandeepdhillon123 <amandeep24397@gmail.com>
2023-04-08 14:05:16 +0530

    men Page

---
+++ javacript/5.LocalStorage/men/men.js
@@ -0,0 +1,86 @@
+
+let mensData = [
+            {
+                img:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+                name:"rahit",
+                price:"$234",
+                strikedoffprice:"$20"
+            },
+            {
+                img:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+                name:"rahit",
+                price:"$234",
+                strikedoffprice:"$20"
+            },
+            {
+                img:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+                name:"rahit",
+                price:"$234",
+                strikedoffprice:"$20"
+            },
+            {
+                img:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+                name:"rahit",
+                price:"$234",
+                strikedoffprice:"$20"
+            },
+            {
+                img:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+                name:"rahit",
+                price:"$234",
+                strikedoffprice:"$20"
+            },
+            {
+                img:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+                name:"rahit",
+                price:"$234",
+                strikedoffprice:"$20"
+            },
+            {
+                img:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+                name:"rahit",
+                price:"$234",
+                strikedoffprice:"$20"
+            },
+            {
+                img:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+                name:"rahit",
+                price:"$234",
+                strikedoffprice:"$20"
+            },
+            
+  ]; 
+
+
+  mensData.map((ele) =>{
+
+    let div=document.createElement("div");
+      div.setAttribute("class","div")
+
+    let img=document.createElement("img");
+    img.src=ele.img;
+    img.setAttribute("class","img")
+
+    console.log(img)
+    img.setAttribute("class","img")
+
+    let name=document.createElement("h5");
+    name.innerText=ele.name
+    name.setAttribute("class","h5")
+    
+    let div2=document.createElement("div");
+    div2.setAttribute("class","div2")
+    let price=document.createElement("p");
+    price.innerText=ele.price
+    let strikedoffprice=document.createElement("p");
+    strikedoffprice.innerText=ele.strikedoffprice
+
+    div2.append(price,strikedoffprice)
+
+  div.append(img,name,div2)
+
+  document.querySelector(".container").append(div)
+
+
+
+  })
