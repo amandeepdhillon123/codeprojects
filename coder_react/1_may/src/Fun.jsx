4e2d97f5e12317b6e4fe0a0b6aa8b99b9592298e
amandeepdhillon123 <amandeep24397@gmail.com>
2023-05-01 09:48:02 +0530

    react basics

---
+++ coder_react/1_may/src/Fun.jsx
@@ -0,0 +1,20 @@
+
+
+function Hello(){
+
+    return <h1> hello friends</h1>
+}
+
+
+// function Demo(){
+//     return(
+//         <div>
+//             <h1>hello jii</h1>  
+//             <h1> ram ram </h1>
+//         </div>
+//     )
+// }
+
+// export default {Hello,Demo};
+
+export default Hello;
