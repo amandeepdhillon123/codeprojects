8249583819533df2fc5f40ecdeef51ed4e63351b
amandeepdhillon123 <amandeep24397@gmail.com>
2023-05-05 17:46:30 +0530

    dice ,

--- javacript/6.how to js Works/throw_dice/index.js
+++ javacript/6.how to js Works/throw_dice/index.js
@@ -1,92 +1,147 @@
-document.querySelector("button").addEventListener("click", myFun);
 
 
-function myFun() {
+
+ document.querySelector("button").addEventListener("click",myFun)
+
+
+ function myFun(){
+
   // console.log("amandeep")
-  
 
-  let diceA = Math.floor(Math.random() * 6 + 1);
-  let diceB = Math.floor(Math.random() * 6 + 1);
-  let diceC = Math.floor(Math.random() * 6 + 1);
-  console.log(diceA, diceB, diceC);
+  let diceA = Math.floor(Math.random() *6 + 1)
+  let diceB = Math.floor(Math.random() *6 + 1)
+  let diceC = Math.floor(Math.random() *6 + 1)
 
-  let member1 = (document.querySelector(".member-1").textContent = diceA);
-  let member2 = (document.querySelector(".member-2").textContent = diceB);
-  let member3 = (document.querySelector(".member-3").textContent = diceC);
 
-  // console.log(member1,member2,member3)
-  // winner --------------------------------------------------------------
+  console.log(diceA,diceB,diceC)
 
-  if (member1 > member2 && member1 > member3) {
-    document.querySelector(".member-1").style.backgroundColor = "green";
-    let win = document.querySelector(".A").innerText;
-  
-    document.querySelector(".winner").innerText = `${win}`;
-  } else if (member2 > member1 && member2 > member3) {
-    document.querySelector(".member-2").style.backgroundColor = "green";
-    let win = document.querySelector(".B").innerText;
-    document.querySelector(".winner").innerText = `${win}`;
-   
-  } else {
-    document.querySelector(".member-3").style.backgroundColor = "green";
-    let win = document.querySelector(".C").innerText;
-    document.querySelector(".winner").innerText = `${win}`;
-    
-  }
+  let member1=document.querySelector('.member-1').textContent= diceA
+  let member2=document.querySelector('.member-2').textContent= diceB
+  let member3=document.querySelector('.member-3').textContent= diceC
+ 
 
-//   // 2nd    -------------------------------------------------------------
+  // winner 
 
-  if ((member1 > member2 && member1< member3) || (member1> member3&& member1 < member2)) {
-    document.querySelector(".member-1").style.backgroundColor = "yellow";
-  } else if (
-    (member2 > member1 &&  member2 <  member3) ||
-    ( member2 >  member3 &&  member2 <  member1)
-  ) {
-    document.querySelector(".member-2").style.backgroundColor = "yellow";
-  } else {
-    document.querySelector(".member-3").style.backgroundColor = "yellow";
-  }
+  if(member3> member2 || member3 > member1)
+  {
+       document.querySelector('.member-3').style.backgroundColor="green"
+      let win=  document.querySelector('.member').innerText;
+      document.querySelector('.winner').innerText=`${win}`
+ }
 
-//    for red   -------------------------------------------------
-    
+ else if(member2> member1 || member2 > member3)
+  {
+       document.querySelector('.member-2').style.backgroundColor="green"
+      let win=  document.querySelector('.member').innerText;
+      document.querySelector('.winner').innerText=`${win}`
+ }
 
-if( member1< member2  &&  member1< member3)
+ else{
+  document.querySelector('.member-1').style.backgroundColor="green"
+  let win=  document.querySelector('.member').innerText;
+  document.querySelector('.winner').innerText=`${win}`
+ }
+ 
+
+
+//  for 2nd   yellow color 
+
+if(( member1 >member2 && member1 < member3) ||(member1 >member3 && member1 < member2))
 {
-    document.querySelector(".member-1").style.backgroundColor = "red";  
+  document.querySelector('.member-1').style.backgroundColor="yellow"
 }
-else if( member2< member1   &&  member2< member1)
+else if(( member2 >member1 && member2 < member3) ||(member2 >member3 && member2 < member1))
 {
-    document.querySelector(".member-2").style.backgroundColor = "red";  
+  document.querySelector('.member-2').style.backgroundColor="yellow"
 }
-else
+
+else{
+  document.querySelector('.member-3').style.backgroundColor="yellow"
+}     
+
+
+// for 3rd  red color
+
+
+if( member1 < member2  && member1 < member3)
 {
-    document.querySelector(".member-3").style.backgroundColor = "red";  
+  document.querySelector('.member-1').style.backgroundColor="red"
+}
+
+else if( member2 < member1 && member2 < member3)
+{
+  document.querySelector('.member-2').style.backgroundColor="red"
+}
+
+else{
+  document.querySelector('.member-3').style.backgroundColor="red"
+}
+
+
+// for draw  blue
+
+if(member1 == member2)
+{
+  document.querySelector('.member-1').style.backgroundColor="blue"
+  document.querySelector('.member-2').style.backgroundColor="blue"
+  document.querySelector('.winner').innerText="draw"
+
+}
+else if(member2 == member3)
+{
+  document.querySelector('.member-2').style.backgroundColor="blue"
+  document.querySelector('.member-3').style.backgroundColor="blue"
+  document.querySelector('.winner').innerText="draw"
+
+}
+
+else if(member3 == member1)
+{
+  document.querySelector('.member-1').style.backgroundColor="blue"
+  document.querySelector('.member-3').style.backgroundColor="blue"
+ document.querySelector('.winner').innerText="draw"
 }
 
 
 
-// for draw 
+ 
 
-if(  member1 == member2 ){
-    document.querySelector(".member-1").style.backgroundColor = "blue";  
-    document.querySelector(".member-2").style.backgroundColor = "blue";  
 
-    document.querySelector(".winner").innerText="Draw"
-  
-}
-else if( member2 ==  member3 ){
 
-    document.querySelector(".member-2").style.backgroundColor = "blue";  
-    document.querySelector(".member-3").style.backgroundColor = "blue"; 
-    document.querySelector(".winner").innerText="Draw" 
-}
 
-else if( member3 == member1){
 
-    document.querySelector(".member-1").style.backgroundColor = "blue";  
-    document.querySelector(".member-3").style.backgroundColor = "blue"; 
-    document.querySelector(".winner").innerText="Draw" 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
