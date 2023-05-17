f7cee1b83f2bfa6b11c794aae93c3deee99abf34
amandeepdhillon123 <amandeep24397@gmail.com>
2023-05-17 10:19:14 +0530

    react

---
+++ react_step_step/17_may/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+//  import Toggle from './Component/Toggle'
+
+// import Form from './Component/Form'
+
+import Render from './Component/Render'
+  function App(){
+    return (
+      <>
+
+          {/* <Toggle/> */}
+        
+
+        {/* <Form/> */}
+        
+
+        <Render/>
+        </>
+    )
+  }
+
+export default App
