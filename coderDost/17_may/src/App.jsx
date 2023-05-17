34553df34cc71aba61f04a8fe48165aafa6f15f5
amandeepdhillon123 <amandeep24397@gmail.com>
2023-05-17 11:33:42 +0530

    conditional rendering

---
+++ coderDost/17_may/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Video from "./component/Video";
+
+function App() {
+  
+  let obj={
+    title:"React -js tutorial",
+    views:"10K",
+    time:"1 year ago",
+    channel:"coderDost",
+     verified:true
+  }
+  return (
+    <div className="App">
+      <Video
+        // title="React -js tutorial"
+        // views="10K"
+        // time="1 year ago"
+        // channel="coderDost"
+  
+        // spread operator 
+        {...obj}
+      />
+      <Video
+        title="Node -js tutorial"
+        views="100K"
+        time=" 2 year ago"
+        channel="coderDost"
+        verified ={false}
+      />
+       <Video
+        title="MongoDb tutorial"
+        views="99K"
+        time=" 2 year ago"
+        channel="coderDost"
+        verified ={true}
+      />
+    </div>
+  );
+
+ 
+}
+
+export default App;
