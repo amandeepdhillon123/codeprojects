e827277548ebfced6dcd239f1e226b3d959160f3
amandeepdhillon123 <amandeep24397@gmail.com>
2023-05-18 10:57:47 +0530

    react

---
+++ react_step_step/18_may/src/Component/Rerender.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class Rerender extends Component {
+    constructor(){
+        super();
+        this.state={
+            email:'xyz@gmial.com'
+        }
+    }
+  render() {
+  console.log(this.state.email)
+    // console.log("class update",this.props)
+    return (
+        <>
+      <div>Rerender</div>
+      <button onClick={() =>this.setState({email:"ram2gmail"})}>update email</button>
+      </>
+    )
+  }
+}
+
+export default Rerender
