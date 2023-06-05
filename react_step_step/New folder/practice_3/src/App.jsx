b5825b62fb453d077a3d11bd57ccbcd8ef2accbd
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-05 08:52:14 +0530

    Toggle

--- react_step_step/New folder/practice_3/src/App.jsx
+++ react_step_step/New folder/practice_3/src/App.jsx
@@ -2,18 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Input from './component/Input'
+// import Input from './component/Input'
+import Toggle from './component/Toggle'
 
-
+Toggle
 // import Student from './component/Student'
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
    <>
   {/* <Student name = "aman"/> */}
 
-  <Input/>
+  {/* <Input/> */}
+
+    <Toggle/>
    </>
   )
 }
