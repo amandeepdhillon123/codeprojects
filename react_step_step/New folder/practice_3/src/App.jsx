692eb41c362d52d4438954b20176e0717c5eefea
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-05 08:40:59 +0530

    react input box value

--- react_step_step/New folder/practice_3/src/App.jsx
+++ react_step_step/New folder/practice_3/src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Input from './component/Input'
 
 
-import Student from './component/Student'
+// import Student from './component/Student'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <>
-  <Student name = "aman"/>
+  {/* <Student name = "aman"/> */}
+
+  <Input/>
    </>
   )
 }
