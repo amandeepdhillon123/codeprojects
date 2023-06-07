c9752ed55eacb4ea938973f69014fbe5503e3da8
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-07 07:41:01 +0530

    reuse component

--- react_step_step/New folder/practice_5/src/App.jsx
+++ react_step_step/New folder/practice_5/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Nested from './component/Nested'
+// import Nested from './component/Nested'
+import Reuse from './component/Reuse'
 function App() {
   const[sur,surName]=useState("am")
 
   return (
   <>
-<Nested/>
+{/* <Nested/> */}
+
+<Reuse/>
   </>
   )
 }
