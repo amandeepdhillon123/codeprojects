dfb43b1a1295aee6ed6c8b8cda2b57d54716b640
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-08 07:05:38 +0530

    dom

---
+++ interviews/js/js2/Dom/iindex.js
@@ -0,0 +1,11 @@
+// // Creating a new element and appending it to an existing element
+// var newElement = document.createElement('div');
+// newElement.textContent = 'New element';
+// elementById.appendChild(newElement);
+// // Removing an element
+// elementBySelector.remove();
+// // Removing a child element from a parent element
+// var parentElement = document.querySelector('.parentElement');
+// var childElement = document.querySelector('.childElement');
+// parentElement.removeChild(childElement);
+// 
