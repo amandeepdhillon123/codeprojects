ae4f43582ed45c37cb6c4199ae1f640d2b6600ff
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-11 17:08:40 +0530

    paramenters send in props

--- react_step_step/New folder/practice_8/src/App.jsx
+++ react_step_step/New folder/practice_8/src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import './App.css'
-import Classs from './component/Classs '
+// import Classs from './component/Classs '
 // import Click from './component/Click'
 // import Function from './component/Function'
+import Props from './component/Props'
 function App() {
   return (
     <div>
  
      {/* <Click/> */}
      {/* <Function/> */}
-     <Classs/>
+     {/* <Classs/> */}
 
+ <Props/>
      
       
     </div>
