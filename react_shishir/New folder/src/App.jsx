89cbd9830c325e02836aa9dca23fc0c8cd3dbacb
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-11 09:38:13 +0530

    event handling

--- react_shishir/New folder/src/App.jsx
+++ react_shishir/New folder/src/App.jsx
@@ -4,15 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Pro1 from './component/Pro1'
 
-import FuncEvent from './component/FuncEvent'
-
+// import FuncEvent from './component/FuncEvent'
+import ClaEvent from './component/ClaEvent'
 function App() {
 
 
   return (
    <>
     {/* <Pro1/> */}
-    <FuncEvent/>
+    {/* <FuncEvent/> */}
+
+    <ClaEvent/>
    </>
   )
 }
--- react_shishir/New folder/src/component/FuncEvent.jsx
+++ react_shishir/New folder/src/component/FuncEvent.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 
 const FuncEvent = () => {
 
-    function show(name){
-        console.log(name)
+    // function show(name){
+    //     console.log(name)
+    // }
+
+    function show(e)
+    {
+        console.log(e.target.value)
     }
   return (
     <>
    <h1>Use event in functional component</h1>
-   {/* <button onClick={show}>submit</button> */}
+   <input type="text" onChange={show} />
+    <button>submit</button>
 
-     <button onClick={()=>show("deep Dhillon")}>submit</button>
+   {/* <button onClick={show}>submit</button> */}
+   
+     {/* <button onClick={()=>show("deep Dhillon")}>submit</button> */}
     </>
   )
 }
