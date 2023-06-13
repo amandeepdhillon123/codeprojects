f369594f94193386d4b33db61a4fa7e715c9303c
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-13 07:32:13 +0530

    useState inutn method

--- react_step_step/New folder/practice_10/src/App.jsx
+++ react_step_step/New folder/practice_10/src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './App.css'
 // import Effect from './component/Effect'
 // import Effec1 from './component/Effec1'
-import Sta from './component/Sta'
+// import Sta from './component/Sta'
+
+import Sta1 from './component/Sta1'
 function App() {
   return (
     <>
       {/* <Effec1/> */}
-      <Sta/>
+      {/* <Sta/> */}
+
+      <Sta1/>
     </>
   )
 }
