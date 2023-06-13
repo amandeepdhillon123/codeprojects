5d1729d81de9a25ccf70e4cd0ed96ee793fa2954
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-13 21:44:37 +0530

    reuse compinnet

--- react_step_step/New folder/practice_12/src/App.jsx
+++ react_step_step/New folder/practice_12/src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import './App.css'
 
 // import List from './component/List'
-import Nested from './component/Nested'
+// import Nested from './component/Nested'
+import Reuse from './component/Reuse'
 function App() {
   return (
     <>
      {/* <List/> */}
 
-     <Nested/>
+     {/* <Nested/> */}
+
+     <Reuse/>
     </>
   )
 }
