4da9dbcc6c784fad4d13159e19294e4381540998
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-13 23:06:41 +0530

    useMemo hook

--- react_step_step/New folder/practice_12/src/App.jsx
+++ react_step_step/New folder/practice_12/src/App.jsx
@@ -5,7 +5,8 @@ import Frag from './component/Frag'
 // import Nested from './component/Nested'
 // import Reuse from './component/Reuse'
 //  import Lift from './component/Lift'
- import Pure from './component/Pure'
+//  import Pure from './component/Pure'
+ import Memo from './component/Memo'
 function App() {
   return (
     <>
@@ -18,7 +19,8 @@ function App() {
 
      {/* <Lift/> */}
 
-     <Pure/>
+     {/* <Pure/> */}
+     <Memo/> 
 
     </>
   )
