2e33000ad50a4ffd3fc378e459ac76bffba702de
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-15 04:20:57 +0530

    pure componet

---
+++ react_step_step/New folder/practice_13/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Pure from './component/Pure'
+function App() {
+  return (
+    <>
+  
+  <Pure/>
+    </>
+  )
+}
+
+export default App
---
+++ react_step_step/New folder/practice_13/src/component/Pure1.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Pure1 extends Component {
+  render() {
+    return (
+      <div>Pure1</div>
+    )
+  }
+}
+
+export default Pure1
