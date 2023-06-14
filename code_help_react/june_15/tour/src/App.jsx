6a96cb0c67f532e750cf62087363002a6c8f7106
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-15 02:05:53 +0530

    mini react project

---
+++ code_help_react/june_15/tour/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useState } from 'react'
+import data from './data'
+import Tours from './component/Tours'
+function App() {
+
+  const[tours,setTours]=useState(data)
+
+  function removeTour(fig){
+     const newTour= tours.filter( tour => tour.id !== fig)
+      setTours(newTour)
+    }
+
+    if(tours.length === 0)
+    {
+       return (
+            <div className='refresh'>
+              <h2>No Tours Left</h2>
+               <button className='btn-white' onClick={()=>  setTours(data)}>Refresh</button>
+            </div>
+       )
+    }
+  return (
+    <div className='App'>
+      <Tours tours={tours} removeTour={removeTour}/>
+    </div>
+  )
+}
+
+export default App
---
+++ code_help_react/june_15/tour/src/component/Card.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+function Card({tour,removeTour}) {
+
+   const[readmore,setReadmore]= useState(false)
+    // const description=`${tour.info.substring(0,200)}....`
+  
+
+     const description= readmore ? `${tour.info} `:`${tour.info.substring(0,200)}....`
+  
+    function readmoreHandler(){
+    setReadmore(!readmore)
+  }
+
+ 
+  return (
+    <div className='card'>
+      {/* image */}
+        <img  className='image' src={tour.image} alt="" />
+    
+    {/* tour-details  */}
+
+    <div className='tour-info'>
+    <div className='tour-details'>
+      <h4 className='tour-price'>₹ {tour.price}</h4>
+       <h4 className='tour-name'>{tour.nam}</h4>
+    </div>
+
+    {/* description  */}
+
+    <div className='description'>
+             {description}
+             <span className='read-more' onClick={readmoreHandler}>
+              {
+                readmore ? `Show Less` : ` Read More`
+              }
+             </span>
+    </div>
+    </div>
+
+    
+    <button onClick={()=>removeTour(tour.id)} className='btn-red'>
+      Not Interested
+    </button>
+    </div>
+  )
+}
+
+export default Card
