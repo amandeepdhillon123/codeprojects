1a8116c24a04ef4e3aaa4c2a8356dd65d5ca02ba
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-15 10:20:49 +0530

    ref.js

--- react_step_step/New folder/practice_13/src/App.jsx
+++ react_step_step/New folder/practice_13/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './App.css'
+import Ref from './component/Ref'
 // import Pure from './component/Pure'
 
-import Memo from './component/Memo'
+// import Memo from './component/Memo'
 function App() {
   return (
     <>
   
   {/* <Pure/> */}
 
-  <Memo/>
+  {/* <Memo/> */}
+
+  <Ref/>
     </>
   )
 }
