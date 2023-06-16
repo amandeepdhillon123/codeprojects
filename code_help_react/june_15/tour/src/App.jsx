60212cf4f4d74520d581c34bdb2bb3a7818a992a
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-17 03:15:11 +0530

    tours

--- code_help_react/june_15/tour/src/App.jsx
+++ code_help_react/june_15/tour/src/App.jsx
@@ -1,28 +1,61 @@
-import React from 'react'
-import { useState } from 'react'
-import data from './data'
-import Tours from './component/Tours'
+// import React from 'react'
+// import { useState } from 'react'
+// import data from './data'
+// import Tours from './component/Tours'
+// function App() {
+
+//   const[tours,setTours]=useState(data)
+
+//   function removeTour(fig){
+//      const newTour= tours.filter( tour => tour.id !== fig)
+//       setTours(newTour)
+//     }
+
+//     if(tours.length === 0)
+//     {
+//        return (
+//             <div className='refresh'>
+//               <h2>No Tours Left</h2>
+//                <button className='btn-white' onClick={()=>  setTours(data)}>Refresh</button>
+//             </div>
+//        )
+//     }
+//   return (
+//     <div className='App'>
+//       <Tours tours={tours} removeTour={removeTour}/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React, { useState } from 'react'
+  import data from './data'
+  import Tours from './component/Tours'
 function App() {
 
-  const[tours,setTours]=useState(data)
+  const[tours,setTours]= useState(data)
 
-  function removeTour(fig){
-     const newTour= tours.filter( tour => tour.id !== fig)
-      setTours(newTour)
-    }
+  function removeTour(id)
+  {
+     const newtour= tours.filter( tour => tour.id !== id)
+     setTours(newtour)
+  }
 
-    if(tours.length === 0)
-    {
-       return (
-            <div className='refresh'>
-              <h2>No Tours Left</h2>
-               <button className='btn-white' onClick={()=>  setTours(data)}>Refresh</button>
-            </div>
-       )
-    }
+  if(tours.length === 0)
+      {
+         return (
+              <div className='refresh'>
+                 <h2>No Tours Left</h2>
+                 <button className='btn-white' onClick={()=>  setTours(data)}>Refresh</button>
+               </div>
+          )
+       }
+
   return (
     <div className='App'>
-      <Tours tours={tours} removeTour={removeTour}/>
+         <Tours tours={tours} removeTour={removeTour}/>
     </div>
   )
 }
