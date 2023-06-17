adfb9295c0556ebcc54da437b5f0390bc90e3a82
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-17 17:24:28 +0530

    profile

--- step_node/17_june/Six.js
+++ step_node/17_june/Six.js
@@ -9,12 +9,14 @@ app.get('/profile',(req,resp)=>{
     const user={
         name:"Amandeep",
 
-        email :"aman@gmail.com"
+        email :"aman@gmail.com",
+
+        skills :['php','js','c++']
     }
 
     resp.render("profile", {user})
 })
-app.listen(2231)
+app.listen(2232)
 
 
 
