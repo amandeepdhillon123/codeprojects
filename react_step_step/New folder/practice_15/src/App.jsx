0a6baa5831c77b180bc3521b4372f99c9b50c081
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-17 13:56:38 +0530

    page not found

--- react_step_step/New folder/practice_15/src/App.jsx
+++ react_step_step/New folder/practice_15/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Nav from "./assets/Nav";
 import Home from "./component/Home";
 import About from "./component/About";
+import PageNot from "./component/PageNot";
 import {Routes ,Route } from "react-router-dom";
 function App() {
   return(
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/" Component={Home}></Route>
       <Route path="/about" Component={About}></Route>
+      <Route path="*" Component={PageNot}></Route>
     </Routes>
     </>
   ) 
