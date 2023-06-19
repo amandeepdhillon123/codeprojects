8987b337eb46729b8315d9df315b1773aa478c72
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-19 12:04:39 +0530

    insert data in nodejs

--- step_node/practice_2/index.js
+++ step_node/practice_2/index.js
@@ -2,12 +2,25 @@
 
 //  read data forn mongo=db 
 
-let main = async()=>{
+// let main = async()=>{
 
-    let data= await dbConnect();
-      data= await data.find({}).toArray()
+//     let data= await dbConnect();
+//       data= await data.find({}).toArray()
 
-      console.log(data)
-}
+//       console.log(data)
+// }
 
-main();
+// main();
+
+ 
+//  insertdata  from mongodb
+
+  let  insert = async ()=>{
+     
+    let data = await dbConnect();
+    // console.log(data)
+    data = await data.insertOne({name:"rohit", age:"67"})
+     console.log(data)
+  }
+
+  insert();
