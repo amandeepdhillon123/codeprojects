bfb8885b23309e97a87949f88763cb5c5eb0d4fb
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-20 01:23:32 +0530

    main.js

--- step_node/practice_2/main.js
+++ step_node/practice_2/main.js
@@ -5,7 +5,7 @@ const express= require('express');
 const app= express();
 
 app.use(express.json())
-app.listen(2240)
+app.listen(2249)
 
 app.post('/create',async(req,resp)=>{
 
@@ -18,4 +18,30 @@ app.post('/create',async(req,resp)=>{
     // console.log(result)
 
     resp.send(result)
+});
+
+app.get('/list',async(req,resp)=>{
+   
+    let data = await products.find();
+    resp.send(data)
+})
+
+app.delete('/delete/:_id',async(req,resp)=>{
+    //  console.log(req.params)
+    //  resp.send("done")
+
+    let data = await products.deleteOne(req.params);
+    resp.send(data)
+})
+
+
+
+app.put('/update/:_id',async(req,resp)=>{
+    //  console.log(req.params)
+    //  resp.send("done")
+
+    // let data = await products.updateOne({name:'m3'},{$set:{name:"Dhillon"}})
+
+    let data = await products.updateOne(req.params,{$set: req.body})
+    resp.send(data)
 })
