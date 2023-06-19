9b38bed93de992b19d5bc26a44d7d09a7cb98af2
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-19 13:17:35 +0530

    data inserted

--- step_node/practice_2/api.js
+++ step_node/practice_2/api.js
@@ -4,10 +4,37 @@ const dbConnect= require('./mongodb')
 
 const app= express();
 
+ app.use(express.json())
+  
+//   get method  api 
 app.get('', async(req,resp)=>{
     let data= await dbConnect();
     data= await data.find({}).toArray();
     resp.send(data)
 })
 
-app.listen(2227)
+app.post('',async(req,resp)=>{
+    //  resp.send({name:"jaatram"})
+
+    //  console.log(req.body)
+    //  resp.send("jai ho")
+
+       let data =  await dbConnect()
+       data =  await data.insertOne(req.body);
+
+       resp.send(data)
+})
+
+
+
+
+
+
+
+app.listen(2228)
+
+
+
+//  post api method  
+
+// app.post('',)
