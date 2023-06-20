ec02adc16e86e5753760ce8b017e6992a79f32af
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-20 19:21:15 +0530

    cors issue

--- code_help_react/june_15/6.E-commerce/backend/index.js
+++ code_help_react/june_15/6.E-commerce/backend/index.js
@@ -1,9 +1,13 @@
 
 const express = require('express')
   require('./db/config');
+
+  const cors= require('cors')
   const Users = require('./db/User')
 const app= express();
   app.use(express.json());
+
+  app.use(cors());
 
 app.post('/register',async(req,resp)=>{
 
