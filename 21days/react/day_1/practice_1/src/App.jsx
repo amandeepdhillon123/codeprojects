8f9cf4c5508d7d4a698054595f930032726c5dc6
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-24 00:49:55 +0530

    fuction component

---
+++ 21days/react/day_1/practice_1/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Function from './component/Function'
+function App() {
+
+
+  return (
+    <>
+  <Function/>
+    </>
+  )
+}
+
+export default App
