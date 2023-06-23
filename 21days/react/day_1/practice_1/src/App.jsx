8c72678ed4582c46784d4cf93e0a476eeabdb20c
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-24 00:54:56 +0530

    Class component

--- 21days/react/day_1/practice_1/src/App.jsx
+++ 21days/react/day_1/practice_1/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Function from './component/Function'
+import { Class } from './component/Class'
+// import Function from './component/Function'
 function App() {
 
 
   return (
     <>
-  <Function/>
+  {/* <Function/> */}
+
+{/* { Function()} */}
+
+<Class/>
     </>
   )
 }
---
+++ 21days/react/day_1/practice_1/src/component/Class.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+import { Component } from "react";
+
+export class Class extends Component{
+    
+    render() {
+
+        return(
+            <h1> hello I am Class component</h1>
+        )
+    }
+}
+
