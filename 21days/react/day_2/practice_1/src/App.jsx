25bbc959e46dec6b3f05b3677d5c9d242d9ed346
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-25 04:25:29 +0530

    shoyuld component update

--- 21days/react/day_2/practice_1/src/App.jsx
+++ 21days/react/day_2/practice_1/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
    {/* <CLASS/> */}
    {/* <Didmount/> */}
 
-   <Render/>
+   <Render ram="shyamxcxcxzcxzczczx"/>
     </>
   )
 }
--- 21days/react/day_2/practice_1/src/component/Render.jsx
+++ 21days/react/day_2/practice_1/src/component/Render.jsx
@@ -10,6 +10,20 @@ export class Render extends Component {
         console.log("constructor")
      
     }
+    componentDidUpdate(preProp,preState,snapShot){
+        console.log("did update", preState.num)
+        console.log(preProp)
+    }
+
+    shouldComponentUpdate(){
+        if(this.state.num > 5 &&  this.state.num < 10 )
+        {
+              return true;
+        }
+        else{
+            return false;
+        }
+    }
 
     componentDidMount(){
         console.log("didmount")
@@ -19,6 +33,7 @@ export class Render extends Component {
         console.log("render")
     return (
       <div>
+        <h2>{this.props.ram}</h2>
          <h1>{this.state.num}</h1>
          <button onClick={()=> this.setState({num: this.state.num + 1})}>update</button>
       </div>
