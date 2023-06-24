bdfb93ad7104a3f8af88457cfe6ba5c0beb2592e
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-25 03:49:27 +0530

    constructor life cycle method

---
+++ 21days/react/day_2/practice_1/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import CLASS from './component/CLASS'
+function App() {
+
+
+  return (
+    <>
+   <CLASS/>
+    </>
+  )
+}
+
+export default App
