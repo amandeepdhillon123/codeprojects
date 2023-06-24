97a481306537ed10775ab7da13acfbf45596b470
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-25 02:14:24 +0530

    tours mini project

--- code_help_react/june_15/2.tour/src/App.jsx
+++ code_help_react/june_15/2.tour/src/App.jsx
@@ -1,35 +1,3 @@
-// import React, { useState } from 'react'
-//   import data from './data'
-//   import Tours from './component/Tours'
-// function App() {
-
-//   const[tours,setTours]= useState(data)
-
-//   function removeTour(id)
-//   {
-//      const newtour= tours.filter( tour => tour.id !== id)
-//      setTours(newtour)
-//   }
-
-//   if(tours.length === 0)
-//       {
-//          return (
-//               <div className='refresh'>
-//                  <h2>No Tours Left</h2>
-//                  <button className='btn-white' onClick={()=>  setTours(data)}>Refresh</button>
-//                </div>
-//           )
-//        }
-
-//   return (
-//     <div className='App'>
-//          <Tours tours={tours} removeTour={removeTour}/>
-//     </div>
-//   )
-// }
-
-// export default App
-
 import React, { useState } from 'react'
 import data from './data'
 import Tours from './component/Tours'
@@ -56,9 +24,7 @@ function App() {
     )
 
   }
-        
-        
-  return (
+return (
     <div>
    <Tours tours={tours} removeTour={removeTour}/>
     </div>
--- code_help_react/june_15/2.tour/src/component/Tours.jsx
+++ code_help_react/june_15/2.tour/src/component/Tours.jsx
@@ -1,25 +1,3 @@
-
-// import React from 'react'
-// import Card from './Card'
-// function Tours({tours ,removeTour}) {
-//   return (
-//     <div className='container'>
-//       <div>
-//            <h2 className='title'> भारत दर्शन </h2>
-//         </div>
-//         <div className='cards'>
-//       {
-//         tours.map((tour)=>{
-//            return <Card key={tour.id}  tour={tour} removeTour={removeTour}/>
-//         })
-//       }
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Tours
-
 import React from 'react'
 import Card from './Card'
 
