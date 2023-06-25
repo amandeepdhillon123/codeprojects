2ba043f6805904cf1192bca9a2f65469c42d46e2
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-25 10:54:51 +0530

    css in react

--- 21days/react/day_2/practice_1/src/App.jsx
+++ 21days/react/day_2/practice_1/src/App.jsx
@@ -2,17 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
- import Effect from './component/Effect'
+//  import Effect from './component/Effect'
 // import One from './component/one'
 //  import Render from './component/Render'
 // import Didmount from './component/Didmount'
 // import CLASS from './component/CLASS'
+ import Style from './component/Style'
 function App() {
 
 
   return (
     <>
-    <Effect/>
+    <Style/>
+    {/* <Effect/> */}
     {/* <One/> */}
    {/* <CLASS/> */}
    {/* <Didmount/> */}
---
+++ 21days/react/day_2/practice_1/src/component/Style.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './custom.module.css'
+function Style() {
+  return (
+    <div>
+   <h1 style={{color: "green"}}>hello amandeep</h1>
+   <h2 className={style.success}> helo ji kive ho sare</h2>
+
+    </div>
+  )
+}
+
+export default Style
