d716d2a31a75b5d20f1ba279006b2a18c3e92a5a
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-26 19:03:38 +0530

    pure component

--- 21days/react/day_3/src/App.jsx
+++ 21days/react/day_3/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import User1 from './component/User1'
+// import User1 from './component/User1'
+import Pure from './component/Pure'
 function App() {
 
 
   return (
     <>
-<User1/>
+{/* <User1/> */}
+
+   <Pure/>
 
     </>
   )
