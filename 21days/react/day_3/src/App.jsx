e4455b8b5c728972383532fba71ce78834636ba7
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-26 20:32:12 +0530

    uncontrooled component

--- 21days/react/day_3/src/App.jsx
+++ 21days/react/day_3/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Uncontroll from './component/Uncontroll'
 // import REC from './component/REC'
 // import ForwardRe from './component/ForwardRef'
- import Controlled from './component/Controlled'
+//  import Controlled from './component/Controlled'
 // import Ref from './component/Ref'
 // import Memo from './component/Memo'
 // import User1 from './component/User1'
@@ -14,7 +15,8 @@ function App() {
 
   return (
     <>
-    <Controlled/>
+    <Uncontroll/>
+    {/* <Controlled/> */}
     {/* <ForwardRe/> */}
 {/* <User1/> */}
 
