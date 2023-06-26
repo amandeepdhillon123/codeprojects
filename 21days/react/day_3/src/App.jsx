615dcbd115d8b9943dc61ead3fb6e74f1f27ee6e
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-26 19:41:01 +0530

    useRef hook

--- 21days/react/day_3/src/App.jsx
+++ 21days/react/day_3/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Ref from './component/Ref'
+import REC from './component/REC'
+
+// import Ref from './component/Ref'
 // import Memo from './component/Memo'
 // import User1 from './component/User1'
 // import Pure from './component/Pure'
@@ -16,7 +18,7 @@ function App() {
    {/* <Pure/> */}
    {/* <Memo/> */}
 
-   <Ref/>
+<REC/>
 
     </>
   )
