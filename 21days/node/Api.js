02094648851d18115ad2a9f7bb5b8a0c9a7a0f74
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-28 10:07:42 +0530

    make simple api

---
+++ 21days/node/Api.js
@@ -0,0 +1,24 @@
+const http= require('http')
+const data = require('./data')
+
+// console.log(http)
+
+
+http.createServer((req,resp)=>{
+    resp.writeHead(200,{'content-type' :'application\JSON'})
+ resp.write(JSON.stringify(
+    data
+    
+    // [
+    // {namne:"amandeep", email:"aman@gmail.com"},
+    // {namne:"amandeep", email:"aman@gmail.com"},
+    // {namne:"amandeep", email:"aman@gmail.com"},
+    // {namne:"amandeep", email:"aman@gmail.com"},
+    //  {namne:"amandeep", email:"aman@gmail.com"}, {namne:"amandeep", email:"aman@gmail.com"},
+
+    
+//  ]
+ ))
+ resp.end()
+
+}).listen(2282)
--- 21days/node/data.js
+++ 21days/node/data.js
@@ -1,5 +1,37 @@
-module.exports = function data(req,res){
-    res.write("hello ji aap ksei ho sare")
-    res.end();
-}
+// module.exports = function data(req,res){
+//     res.write("hello ji aap ksei ho sare")
+//     res.end();
+// }
 
+ 
+
+const data =[
+    {name:"amandeep", email:"aman@gmail.com"},
+    {name:"amandeep", email:"aman@gmail.com"},
+    {name:"amandeep", email:"aman@gmail.com"},
+    {name:"amandeep", email:"aman@gmail.com"},
+    {name:"amandeep", email:"aman@gmail.com"},
+    {name:"amandeep", email:"aman@gmail.com"},
+    {name:"amandeep", email:"aman@gmail.com"},
+    {name:"amandeep", email:"aman@gmail.com"},
+
+    {name:"amandeep", email:"aman@gmail.com"},
+
+    {name:"amandeep", email:"aman@gmail.com"},
+    {name:"amandeep", email:"aman@gmail.com"},
+    {name:"amandeep", email:"aman@gmail.com"},
+
+    {name:"amandeep", email:"aman@gmail.com"},
+
+
+     {name:"amandeep", email:"aman@gmail.com"},
+     {name:"amandeep", email:"aman@gmail.com"},
+
+      {name:"amandeep", email:"aman@gmail.com"},
+       {name:"amandeep", email:"aman@gmail.com"},
+       {name:"amandeep", email:"aman@gmail.com"},
+       {name:"amandeep", email:"aman@gmail.com"},
+]
+
+
+module.exports= data;
