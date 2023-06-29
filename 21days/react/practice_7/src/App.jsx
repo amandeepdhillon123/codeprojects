0e798bfec9c0455730398d1d85d4f3b857d0a100
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-29 11:13:04 +0530

    conditional rednerging

--- 21days/react/practice_7/src/App.jsx
+++ 21days/react/practice_7/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Input from './component/Input'
-import Toggle from './component/Toggle'
-import Form from './component/Form'
+// import Input from './component/Input'
+// import Toggle from './component/Toggle'
+// import Form from './component/Form'
+import Render from './component/Render'
 function App() {
 
 
@@ -12,7 +13,8 @@ function App() {
     <>
       {/* <Input/> */}
       {/* <Toggle/> */}
-      <Form/>
+      {/* <Form/> */}
+      <Render/>
     </>
   )
 }
