9415b337f99c883979fbfcad6194c2655789ef47
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-29 10:52:00 +0530

    toggle function in react

--- 21days/react/practice_7/src/App.jsx
+++ 21days/react/practice_7/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Input from './component/Input'
+import Toggle from './component/Toggle'
 function App() {
 
 
   return (
     <>
-      <Input/>
+      {/* <Input/> */}
+      <Toggle/>
     </>
   )
 }
