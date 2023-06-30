28928d153d64bfb566a2738312775082e7380ef8
amandeepdhillon123 <amandeep24397@gmail.com>
2023-06-30 10:37:10 +0530

    put api method

--- 21days/mongodb/Api.js
+++ 21days/mongodb/Api.js
@@ -2,17 +2,26 @@
   const express = require('express');
 
   const app = express();
-
+   app.use(express.json())
   const dbConnect= require('./base');
+//   read data 
+//   app.get("",async(req,resp) =>{
 
-  app.get("",async(req,resp) =>{
+//     let data = await dbConnect();
+//        data  = await data.find().toArray();
 
-    let data = await dbConnect();
-       data  = await data.find().toArray();
+//     resp.send(data)
+//     )
 
-    resp.send(data)
-     
+    app.post('/',async(req,resp) =>{
+    //   console.log(req.body)
+        // resp.send("done")
+        let data = await dbConnect();
+         let result = await data.insertMany(req.body);
+         resp.send(result)
+    })
 
-   
-  })
-  app.listen(2209)
+
+
+
+  app.listen(2211)
