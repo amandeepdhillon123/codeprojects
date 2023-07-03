b5dcfec7d2150dc2979e1444aef450da0350be9a
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-03 10:25:38 +0530

    forEach method

---
+++ 21days/javascript/masai/practice_3/order.js
@@ -0,0 +1,12 @@
+  const number =[1,2,3,4,5,6];
+   let bag = 0
+  const doubleNumber = number.forEach((ele,index,array)=>{
+    //  console.log(ele,index,array)
+       bag = bag + array[1]
+        
+      
+   
+  })
+
+  console.log(bag)
+
