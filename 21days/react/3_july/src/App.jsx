e924e4520426a7c47c90978a0c62474725843f93
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-03 15:37:31 +0530

    State.jsx

--- 21days/react/3_july/src/App.jsx
+++ 21days/react/3_july/src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Event from './component/Event'
-import Fun from './component/Fun'
+// import Event from './component/Event'
+// import Fun from './component/Fun'
+import State from './component/State'
 function App() {
 
 
   return (
     <>
    {/* <Event/> */}
-   <Fun/>
+   {/* <Fun/> */}
+   <State/>
     </>
   )
 }
