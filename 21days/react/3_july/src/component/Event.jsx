a97e95f87525a4755b2f7b4166551183e0d4c696
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-03 15:24:43 +0530

    event

---
+++ 21days/react/3_july/src/component/Event.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+ alert("hello ji kais eho sbhit aaap ")
+function Event() {
+
+    function Handler(){
+         alert 
+         (
+            "ja shree ram"
+         )
+    }
+  return (
+    <div>
+        {/* <button onClick={Handler}>update</button> */}
+
+        {/* <button onClick={Handler()}>update</button> */}
+    
+        {/* <button onClick={ ()=>Handler()}>update</button> */}
+    </div>
+  )
+}
+
+export default Event
