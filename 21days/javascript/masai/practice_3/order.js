8880085f67427923397edd625dbb5fd32b14b416
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-03 10:53:26 +0530

    higher order function

--- 21days/javascript/masai/practice_3/order.js
+++ 21days/javascript/masai/practice_3/order.js
@@ -1,12 +1,35 @@
   const number =[1,2,3,4,5,6];
    let bag = 0
-  const doubleNumber = number.forEach((ele,index,array)=>{
-    //  console.log(ele,index,array)
-       bag = bag + array[1]
+//   const doubleNumber = number.forEach((ele,index,array)=>{
+//     //  console.log(ele,index,array)
+//        bag = bag + array[1]
         
       
    
-  })
+//   })
 
-  console.log(bag)
+//   console.log(bag)
 
+// map function 
+  
+// const num = number.map((ele,index,array)=>{
+     
+//     return array
+// })
+
+// console.log(num)
+
+
+// const num = number.filter((ele,index,array)=>{
+     
+//      return ele  <0
+// })
+
+// console.log(num)
+
+
+// const num = number.reduce((ass,ele,index,array)=>{
+//       return ele + ass > 0
+// },0)
+
+// console.log(num)
