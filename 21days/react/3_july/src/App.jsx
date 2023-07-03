e0c0cce845b9a3224fb1c68e32d60e1f3944dc27
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-03 15:48:13 +0530

    input box

--- 21days/react/3_july/src/App.jsx
+++ 21days/react/3_july/src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Input from './component/Input'
 // import Event from './component/Event'
 // import Fun from './component/Fun'
-import State from './component/State'
+// import State from './component/State'
+
 function App() {
 
 
@@ -12,7 +14,8 @@ function App() {
     <>
    {/* <Event/> */}
    {/* <Fun/> */}
-   <State/>
+   {/* <State/> */}
+   <Input/>
     </>
   )
 }
