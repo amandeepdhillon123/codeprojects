4e743986e7aae5cfb3360f7327880d28c51a100f
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-03 16:12:58 +0530

    basic from destructuring method

--- 21days/react/3_july/src/App.jsx
+++ 21days/react/3_july/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Form from './component/Form'
 // import Input from './component/Input'
 // import Event from './component/Event'
 // import Fun from './component/Fun'
 // import State from './component/State'
-import Toggle from './component/Toggle'
+// import Toggle from './component/Toggle'
 
 function App() {
 
@@ -17,7 +18,8 @@ function App() {
    {/* <Fun/> */}
    {/* <State/> */}
    {/* <Input/> */}
-   <Toggle/>
+   {/* <Toggle/> */}
+    <Form/>
 
     </>
   )
