99819991f7e4134f0d8197bdc5a2a700dbe7c1e6
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-03 10:01:44 +0530

    usinf state object

--- 21days/react/react_1/src/App.jsx
+++ 21days/react/react_1/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import State from './component/state'
-
+// import State from './component/state'
+ import Fun from './component/Fun'
 function App() {
 
 
   return (
     <>
-   <State/>
+   {/* <State/> */}
+   <Fun/>
     </>
   )
 }
