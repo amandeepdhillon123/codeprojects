9bdceeede97dbaadf5abbc3cca25b81fa79661f7
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-03 09:37:21 +0530

    useState hook

---
+++ 21days/react/react_1/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import State from './component/state'
+
+function App() {
+
+
+  return (
+    <>
+   <State/>
+    </>
+  )
+}
+
+export default App
