0e6d98cf1f808de9bfd7ea7a45c581e79d386f40
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-03 15:29:38 +0530

    state in fun

--- 21days/react/3_july/src/App.jsx
+++ 21days/react/3_july/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Event from './component/Event'
+import Fun from './component/Fun'
 function App() {
 
 
   return (
     <>
-   <Event/>
+   {/* <Event/> */}
+   <Fun/>
     </>
   )
 }
