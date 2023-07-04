349a114382454c0f9b65de9aa018320b7cf5503f
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-04 14:48:30 +0530

    : usememo hook

--- 21days/react/react_2/src/App.jsx
+++ 21days/react/react_2/src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Fun1 from './component/fun1'
-import Pure from './component/Pure'
+import Memo from './component/Memo'
+// import Fun1 from './component/fun1'
+// import Pure from './component/Pure'
 // import Array1 from './component/Array1'
 function App() {
 return (
     <>
         {/* <Array1/> */}
         {/* <Fun1/> */}
-        <Pure/>
+        {/* <Pure/> */}
+        <Memo/>
     </>
   )
 }
