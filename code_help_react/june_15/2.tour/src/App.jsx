c01f2bf28a0ca9b5f148dfa40d8241927a1f69fc
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-04 11:07:14 +0530

    tour app

--- code_help_react/june_15/2.tour/src/App.jsx
+++ code_help_react/june_15/2.tour/src/App.jsx
@@ -1,32 +1,75 @@
+// import React, { useState } from 'react'
+// import data from './data'
+// import Tours from './component/Tours'
+// function App() {
+
+//   const[tours,setTours]=useState(data)
+
+//   console.log(tours)
+
+//   function removeTour(id)
+//   {
+//           const netTour= tours.filter(tour => tour.id !== id)
+            
+//           setTours(netTour)
+//   }  
+
+//   if(tours.length ===  0){
+
+//     return(
+//       <div className='refresh'>
+//         <h2>No Tours Left </h2>
+//         <button className='btn-white' onClick={()=>  setTours(data)}>Refresh</button>
+//       </div>
+//     )
+
+//   }
+// return (
+//     <div>
+//    <Tours tours={tours} removeTour={removeTour}/>
+//     </div>
+//   )
+// }
+
+// export default App
+
 import React, { useState } from 'react'
-import data from './data'
 import Tours from './component/Tours'
+import Data from './data'
+
 function App() {
 
-  const[tours,setTours]=useState(data)
+  const[data,setData]= useState(Data)
+  // console.log(data)
 
-  console.log(tours)
-
-  function removeTour(id)
-  {
-          const netTour= tours.filter(tour => tour.id !== id)
-            
-          setTours(netTour)
-  }  
-
-  if(tours.length ===  0){
-
-    return(
-      <div className='refresh'>
-        <h2>No Tours Left </h2>
-        <button className='btn-white' onClick={()=>  setTours(data)}>Refresh</button>
-      </div>
-    )
-
+  function removeTour(id){
+     let newData = data.filter(dat => dat.id !== id)
+  // console.log(newData)
+     setData(newData)
   }
-return (
+
+  //   if(tours.length ===  0){
+
+//     return(
+//       <div className='refresh'>
+//         <h2>No Tours Left </h2>
+//         <button className='btn-white' onClick={()=>  setTours(data)}>Refresh</button>
+//       </div>
+//     )
+
+//   }
+
+if(data.length  === 0)
+{ return (
+  <div className='refresh'>
+    <h2>NO Tours Left</h2>
+    <button className='btn-white' onClick={()=>setData(Data)}>Refrest</button>
+  </div>
+  )
+}
+  return (
     <div>
-   <Tours tours={tours} removeTour={removeTour}/>
+        <Tours data={data} removeTour={removeTour}/>
     </div>
   )
 }
--- code_help_react/june_15/2.tour/src/component/Tours.jsx
+++ code_help_react/june_15/2.tour/src/component/Tours.jsx
@@ -1,16 +1,35 @@
+// import React from 'react'
+// import Card from './Card'
+
+// function Tours({tours,removeTour}) {
+//   return (
+//     <div className='container'>
+//       <div className='title'> <h2> भारत दर्शन </h2>
+//       </div>
+
+//       <div className='cards'>
+//         {
+//           tours.map((tour)=>{
+//              return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>
+//           })
+//         }
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Tours
+
 import React from 'react'
 import Card from './Card'
-
-function Tours({tours,removeTour}) {
+function Tours({data,removeTour}) {
   return (
     <div className='container'>
-      <div className='title'> <h2> भारत दर्शन </h2>
-      </div>
-
+      <div className='title'><h2> भारत दर्शन </h2></div>
       <div className='cards'>
         {
-          tours.map((tour)=>{
-             return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>
+          data.map((tour)=>{
+            return <Card key ={tour.id} {...tour} removeTour={removeTour}/>
           })
         }
       </div>
