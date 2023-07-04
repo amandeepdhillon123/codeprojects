5ea6ca6f7b46a108dcb416ba5c2f18059a457dc0
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-04 16:27:07 +0530

    uncontrolled component

--- 21days/react/react_2/src/App.jsx
+++ 21days/react/react_2/src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Uncontrolled from './component/Uncontrolled'
 // import Ref1 from './component/Ref1'
-  import Ref3 from './component/Ref3'
+  // import Ref3 from './component/Ref3'
 // import Ref2 from './component/Ref2'
 // import Memo from './component/Memo'
 // import Fun1 from './component/fun1'
@@ -11,8 +12,10 @@ import './App.css'
 // import Array1 from './component/Array1'
 function App() {
 return (
-    <>
-        <Ref3/>
+  
+    <>  
+        <Uncontrolled/>
+        {/* <Ref3/> */}
         {/* <Array1/> */}
         {/* <Fun1/> */}
         {/* <Pure/> */}
