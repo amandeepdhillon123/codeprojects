b3882486db21a537b1dc6bdb9140873fa9237b1a
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-06 14:25:16 +0530

    constructor in form

--- js/6_july/index.js
+++ js/6_july/index.js
@@ -8,18 +8,28 @@
 
 // console.log(obj.sayHi())
 
-function NikeProduct(n,p) {
-    let obj={};
+// function NikeProduct(n,p) {
+//     let obj={};
 
-    obj.name=n
-    obj.price=p
+//     obj.name=n
+//     obj.price=p
 
-    return obj;
+//     return obj;
 
-}
+// }
 
-let p1= NikeProduct("shoes", 200)
+// let p1= NikeProduct("shoes", 200)
 
-let p2= NikeProduct("ram",5522)
+// let p2= NikeProduct("ram",5522)
 
-console.log(p2)
+// console.log(p2)
+
+
+// function Nike(n,p){
+//     this.name=n;
+//     this.price=p
+// }
+
+// let obj1= new Nike("shoes", 3300)
+
+// console.log(obj1)
