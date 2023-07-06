b893d9508ebeb259adbc5e2d8c2181550a9acbe0
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-06 14:01:05 +0530

    factor function

---
+++ js/6_july/index.js
@@ -0,0 +1,25 @@
+// let obj= {
+//    name:"aman",
+
+//    sayHi: function(){
+//     console.log("amandeep")
+//    }
+// }
+
+// console.log(obj.sayHi())
+
+function NikeProduct(n,p) {
+    let obj={};
+
+    obj.name=n
+    obj.price=p
+
+    return obj;
+
+}
+
+let p1= NikeProduct("shoes", 200)
+
+let p2= NikeProduct("ram",5522)
+
+console.log(p2)
