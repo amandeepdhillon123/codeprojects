a5d4cb2fe629b4b07fd82ec78f24d64a57fba233
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-06 10:14:23 +0530

    top courses

--- code_help_react/june_15/3.Course/src/components/Navbar.js
+++ code_help_react/june_15/3.Course/src/components/Navbar.js
@@ -1,28 +1,11 @@
 
-
-
-// import React from 'react'
-
-// function Navbar() {
-//   return (
-//     <div>
-//       <nav className='bg-bgDark py-4'>
-//         <h1 className='text-3xl font-bold text-center text-white '>Top Courses</h1>
-//       </nav>
-//     </div>
-//   )
-// }
-
-// export default Navbar
-
-
 import React from 'react'
 
 function Navbar() {
   return (
     <div>
-      <nav>
-        <h1>Top Courses</h1>
+      <nav className='bg-bgDark py-4'>
+        <h1 className='text-3xl font-bold text-center text-white '>Top Courses</h1>
       </nav>
     </div>
   )
