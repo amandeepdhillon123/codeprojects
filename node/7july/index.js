ca9511156953ab60dfedab46b989423adda3d37d
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-07 14:35:34 +0530

    make simple api

--- node/7july/index.js
+++ node/7july/index.js
@@ -1,10 +1,25 @@
 // console.log("amamndeep")
 
+// const http = require('http');
+
+// http.createServer((req,resp)=>{
+// //    resp.write("hello  i am amandeep dhillon")
+
+//    resp.write( "<h1>Hello  Ig am Amandeep Dhillon</h1>")
+//    resp.end()
+// }).listen(2233)
+
+// sim[pple api 
+
+// console.log("amambndeep")
+
 const http = require('http');
-
+ const data = require('./data')
 http.createServer((req,resp)=>{
-//    resp.write("hello  i am amandeep dhillon")
+    resp.writeHead(200,{'content-type' :'application/Json'})
+    resp.write(JSON.stringify(data))
+   
 
-   resp.write( "<h1>Hello  Ig am Amandeep Dhillon</h1>")
-   resp.end()
-}).listen(2233)
+}).listen(2239)
+
+
