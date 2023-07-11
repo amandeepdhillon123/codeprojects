157f417063f09c155ad872ce06402762097491a5
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-11 19:16:54 +0530

    post api method

--- node/11_july'/api.js
+++ node/11_july'/api.js
@@ -1,19 +1,57 @@
 const express = require('express');
 const dbConnect = require('./data');
 
+
 const app = express();
+  app.use(express.json())
 
-
-// const dbConnect= require('./data')
-
-app.get("",async(req,resp)=>{
-
-    let data = await dbConnect();
-    data = await data.find().toArray()
-
-    resp.send(data)
-})
-
-app.listen(2240,()=>{
+app.listen(2252,()=>{
     console.log("resy")
 })
+
+// const dbConnect= require('./data')
+//   read data 
+// app.get("",async(req,resp)=>{
+
+//     let data = await dbConnect();
+//     data = await data.find().toArray()
+
+//     resp.send(data)
+// })
+
+
+// api data
+
+
+
+
+app.post("/",async(req,resp)=>{
+
+   let data = await dbConnect();
+
+   data =await data.insertOne(req.body)
+
+
+   resp.send(send ,"done")
+   
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
