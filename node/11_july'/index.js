62cc7ed28eed4bf1f360f8704b5ca2d1d9ec3317
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-11 17:12:22 +0530

    db connec t

--- node/11_july'/index.js
+++ node/11_july'/index.js
@@ -1,27 +1,42 @@
-// console.log("amandeep")
+
 
 
 const {MongoClient} = require('mongodb')
 
-// console.log(MongoClient)
+
 
 const url ='mongodb://127.0.0.1:27017';
 
-// console.log(url)
+
 
 const client = new MongoClient(url)
 
-// console.log(client)
 
-async function getData(){
+
+async function dbConnect(){
     let result = await client.connect();
     let db = result.db('e-commerce');
 
-    let collection =db.collection('products');
+     return db.collection('products');
 
-    let response = await collection.find({}).toArray();
-
-    console.log(response)
+   
 }
 
-getData()
+// getData().then((data)=>{
+
+//     console.log(data.find().toArray().then((dat)=>{
+//         console.log(dat)
+//     }))
+
+// })
+
+let main = async ()=>{
+
+    let data = await dbConnect();
+
+    data = await data.find().toArray()
+    console.log(data)
+}
+
+main();
+
