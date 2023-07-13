6c39514e7a460f131bf2a7758339e752b4dfdc68
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-13 11:44:25 +0530

    setimeout and interval

--- 21days/javascript/function/index.js
+++ 21days/javascript/function/index.js
@@ -10,6 +10,21 @@
 // console.log(main())
 
 
-let main =()=> console.log("amandeep")
+// let main =()=> console.log("amandeep")
 
-main();
+// main();
+
+
+// higher order function 
+
+
+// (function(r){
+//     console.log(r)
+// })("hello")
+
+function greeting(a){
+    console.log(`${a} Dhillon`)
+}
+
+// setTimeout(greeting,5000,"amandeep")
+// setInterval(greeting,2000,"amandeep")
