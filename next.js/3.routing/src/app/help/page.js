246b72544c0aa69a94fe8cdc7ed75fb68bea54bc
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-14 09:50:36 +0530

    basic routing

---
+++ next.js/3.routing/src/app/help/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function help() {
+  return (
+    <div>
+        <h1> I am  help page</h1>
+    </div>
+  )
+}
+
+export default help
---
+++ next.js/3.routing/src/app/page.js
@@ -0,0 +1,34 @@
+
+// import styles from './page.module.css'
+
+// export default function Home() {
+//   return (
+//     <main className={styles.main}>
+//       <p>home</p>
+//       <User name ="ram"/>
+//       <User name ="shyaam"/>
+//       <User name ="dfcdhgam"/>
+//     </main>
+//   )
+// }
+
+//  const User =(props)=>{
+
+//   return(
+//     <div>
+//       <p>hello ji {props.name}</p>
+//     </div>
+//   )
+// }
+
+import React from 'react'
+
+function Home() {
+  return (
+<main>
+  <h1> hello i am home page</h1>
+</main>
+  )
+}
+
+export default Home
