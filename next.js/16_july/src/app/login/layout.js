0ee412e2bae5fafdb6f69858b79a8dc7f5f88a90
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-17 07:55:46 +0530

    conditional layout

--- next.js/16_july/src/app/login/layout.js
+++ next.js/16_july/src/app/login/layout.js
@@ -1,20 +1,33 @@
+ "use client"
 import Link from "next/link"
 import '../about/about.css'
+
+import { usePathname } from "next/navigation"
 const Layout = ({children}) => {
+
+    const pathName = usePathname()
+    console.log(pathName)
   return (
        <div>
+
+        {
+            pathName !=="/login/teacherLogin" ?
+            <ul className="about-menu">
+            <li>
+                <Link href={"/login"}>Loginmain</Link>
+            </li>
+            <li>
+                <Link href={"/login/studentLogin"}>StudentLogin</Link>
+            </li>
+            <li>
+                <Link href={"/login/teacherLogin"}>TeacherLogin</Link>
+            </li>
+           </ul>
+           :
+           <h1>conditonal layout</h1>
+        }
       
-       <ul className="about-menu">
-        <li>
-            <Link href={"/login"}>Loginmain</Link>
-        </li>
-        <li>
-            <Link href={"/login/studentLogin"}>StudentLogin</Link>
-        </li>
-        <li>
-            <Link href={"/login/teacherLogin"}>TeacherLogin</Link>
-        </li>
-       </ul>
+      
 
        {
         children
