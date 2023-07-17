bdadf9ca9b6a8c35c71f8768d7216035a4690b58
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-17 11:45:56 +0530

    home page

--- Mern_Portfolio/Portfolio/src/pages/Home/Home.jsx
+++ Mern_Portfolio/Portfolio/src/pages/Home/Home.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import Typewriter from "typewriter-effect";
+import "./Home.css";
+import resume from "../../assets/docs/resume.txt"
 const Home = () => {
   return (
-    <div>
-        <h1>Home page</h1>
-    </div>
-  )
-}
+    <>
+      <div className="container-fluid home-container">
+        <div className="container home-content">
+          <h1>Hi I'm a</h1>
+          <h2>
+            <Typewriter
+              options={{
+                strings: ["FullStack Developer !", "Mern Stack Developer ! "],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
+          <div className="home-buttons">
+           
+          <button className="btn btn-hire">Hire Me</button>
+          <a className="btn btn-cv" href={resume} download="your_name.pdf">My Resume</a>
+          </div>
+        </div> 
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
