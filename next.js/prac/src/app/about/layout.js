bec6098e14780b367a5722f77e6e291f454e1a16
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-18 11:36:58 +0530

    conditiola layoyut

--- next.js/prac/src/app/about/layout.js
+++ next.js/prac/src/app/about/layout.js
@@ -1,20 +1,30 @@
 
-import React from 'react'
+"use client"
 import Link from 'next/link'
+import {usePathname} from 'next/navigation'
 const Log = ({children}) => {
+
+    const pathname = usePathname();
   return (
+   
     <div>
+         {
+        pathname !== "/about/aboutStudent" ?
         <ul>
-            <li>
-                <Link href="/about">about</Link>
-            </li>
-            <li>
-                <Link href="/about/aboutCollege">abotCollege</Link>
-            </li>
-            <li>
-                <Link href="/about/aboutStudent">aboutStudent</Link>
-            </li>
-        </ul>
+        <li>
+            <Link href="/about">about</Link>
+        </li>
+        <li>
+            <Link href="/about/aboutCollege">abotCollege</Link>
+        </li>
+        <li>
+            <Link href="/about/aboutStudent">aboutStudent</Link>
+        </li>
+    </ul> :
+      <h1> hello ji condtitinla lgi hoi hai</h1>
+    }
+       
+
            {children}
     </div>
   )
--- next.js/prac/src/app/login/layout.js
+++ next.js/prac/src/app/login/layout.js
@@ -1,19 +1,29 @@
-import React from 'react'
+"use client"
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 const Log = ({children}) => {
+
+    const pathname = usePathname();
   return (
     <div>
+        { 
+        pathname !== "/login/teacherLogin" ?
+
         <ul>
-            <li>
-                <Link href="/login">login</Link>
-            </li>
-            <li>
-                <Link href="/login/studentLogin">student login</Link>
-            </li>
-            <li>
-                <Link href="/login/teacherLogin">teacherlogin</Link>
-            </li>
-        </ul>
+        <li>
+            <Link href="/login">login</Link>
+        </li>
+        <li>
+            <Link href="/login/studentLogin">student login</Link>
+        </li>
+        <li>
+            <Link href="/login/teacherLogin">teacherlogin</Link>
+        </li>
+    </ul> :
+      <h1> conditional layout </h1>
+
+        }
+      
            {children}
     </div>
   )
