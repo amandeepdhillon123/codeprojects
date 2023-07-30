32497b37a1b4cfa8871100ad5d206267b743ef07
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-30 18:13:52 +0530

    authentication

---
+++ Authentication/client/src/App.js
@@ -0,0 +1,14 @@
+
+import Login from "./components/Login";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Login/>
+    </>
+  );
+}
+
+export default App;
---
+++ Authentication/client/src/components/Login.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import "./mix.css";
+const Login = () => {
+  //   state for toogle hide and show
+
+  const [passShow, setPassShow] = useState(false);
+  return (
+    <>
+      <section>
+        {/* upper title  */}
+        <div className="form_data">
+          <div className="form_heading">
+            <h1>Welcomw Back, Log In</h1>
+            <p>Hi, we are you glad you are back. Please login</p>
+          </div>
+
+          {/* form data  */}
+
+          <form>
+            {/* login  */}
+            <div className="form_input">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="enter your email"
+              />
+            </div>
+
+            {/* password  */}
+            <div className="form_input">
+              <label htmlFor="password">Password</label>
+              <div className="two">
+                <input
+                  type={passShow ? "text" : "password"}
+                  name="password"
+                  id="password"
+                  placeholder="enter your  password"
+                />
+                {/* toggle  */}
+                <div
+                  className="showpass"
+                  onClick={() => setPassShow(!passShow)}
+                >
+                  {passShow ? "hide" : "show"}
+                </div>
+              </div>
+            </div>
+            {/* button  */}
+            <button className="btn">Login</button>
+            <p>Don't have an Account? Sign Up</p>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
