55d76d5f829d9573796b9dfcdced8c7fcbfcb5eb
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-01 23:27:18 +0530

    client side of crud

--- crud/client/src/component/Home.js
+++ crud/client/src/component/Home.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import CreateIcon from '@mui/icons-material/Create';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { NavLink } from "react-router-dom";
 const Home = () => {
+  const [getuserdata, setUserdata] = useState([]);
+  console.log(getuserdata.data)
+    //  console.log(ele.name)
+
+  const getpdata = async(e) => {
+    // e.preventDefault();
+   
+    const resp = await fetch("http://localhost:4000/getdata",{
+        method:"GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+       
+
+    });
+
+    const data = await resp.json();
+    if (resp.status === 422 || !data) {
+      console.log("error ");
+      alert("error");
+
+  } else {
+    setUserdata(data)
+      // history.push("/")
+      // setUdata(data)
+      // alert("data added")
+      console.log(" get Data");
+
+  }
+  };
+
+  useEffect(()=>{
+    getpdata()
+  },[])
   return (
     <div className="mt-5">
       <div className="container">
         <div className="add_btn mt-2 mb-2">
-          <button className="btn btn-primary"> Add Data</button>
+        <NavLink to="/register" className="btn btn-primary">Add Data</NavLink>  
         </div>
         <table class="table">
           <thead>
@@ -21,43 +57,40 @@ const Home = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>meet</td>
-              <td>meet@gmail.com</td>
-              <td>Webdeveloper</td>
-              <td>7897366390</td>
-              <td className="d-flex justify-content-between">
-                <button className="btn btn-success">
-                 <RemoveRedEyeIcon/>
-                </button>
-                <button className="btn btn-primary">
-                 <CreateIcon/>
-                </button>
-                <button className="btn btn-danger">
-                 <DeleteOutlineIcon/>
-                </button>
-              </td>
-            </tr>
+       
+        
+        
+          { getuserdata.data &&
+         getuserdata.data.map((element,id)=>
+          
+            <>
+              <tr>
+            <th scope="row">{id + 1}</th>
+            <td>{element.name}</td>
+            <td>{element.email}</td>
+            <td>{element.work}</td>
+            <td>{element.mobile}</td>
+            <td className="d-flex justify-content-between">
+            <NavLink to={`view/${element._id}`}>
+            <button className="btn btn-success">
+               <RemoveRedEyeIcon/>
+              </button></NavLink>  
+              <button className="btn btn-primary">
+               <CreateIcon/>
+              </button>
+              <button className="btn btn-danger">
+               <DeleteOutlineIcon/>
+              </button>
+            </td>
+          </tr>
+            </>
+          
+          
+        )
 
-            <tr>
-              <th scope="row">2</th>
-              <td>meet</td>
-              <td>meet@gmail.com</td>
-              <td>Webdeveloper</td>
-              <td>7897366390</td>
-              <td className="d-flex justify-content-between">
-                <button className="btn btn-success">
-                 <RemoveRedEyeIcon/>
-                </button>
-                <button className="btn btn-primary">
-                 <CreateIcon/>
-                </button>
-                <button className="btn btn-danger">
-                 <DeleteOutlineIcon/>
-                </button>
-              </td>
-            </tr>
+        } 
+            
+
           </tbody>
         </table>
       </div>
