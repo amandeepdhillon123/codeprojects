ab77afdc68578e7dc9b1383941634ed178aa0717
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-03 09:40:45 +0530

    baisc routing in next js

---
+++ next.js/3_Aug/src/app/page.js
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+import About from './about/page'
+import Login from './login/page'
+const page = () => {
+  return (
+    <div>
+      <h1>home</h1>
+       <About/>
+       <Login/>
+    </div>
+  )
+}
+
+export default page
+
+
