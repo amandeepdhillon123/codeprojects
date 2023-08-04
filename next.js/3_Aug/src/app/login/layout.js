e8a24c083ff618ed12f6732cccd4ba594e1580f7
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-04 17:04:41 +0530

    some change d

---
+++ next.js/3_Aug/src/app/login/layout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const layout = ({children}) => {
+  return (
+    <div>
+      
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        { children}
+    </div>
+  )
+}
