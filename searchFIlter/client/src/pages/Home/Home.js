c70664381110d879058ff7db854a2656e4cf8fa1
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-05 18:44:00 +0530

    home page is created

--- searchFIlter/client/src/pages/Home/Home.js
+++ searchFIlter/client/src/pages/Home/Home.js
@@ -1,9 +1,119 @@
-import React from 'react'
-import './home.css'
+import React from "react";
+import "./home.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Dropdown from "react-bootstrap/Dropdown";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="container">
+        <div className="main_div">
+          {/* serach bar and button */}
 
-export default Home
+          <div className="search_add mt-4 d-flex justify-content-between">
+            <div className="search col-lg-4">
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="success" className="search_btn">
+                  Search
+                </Button>
+              </Form>
+            </div>
+
+            {/* add button */}
+
+            <div className="add_btn">
+              <Button variant="primary">
+                {" "}
+                <i class="fa-solid fa-plus"></i>&nbsp; Add User
+              </Button>
+            </div>
+          </div>
+          {/* export,gender,status */}
+
+          <div className="filter_div mt-5 d-flex justify-content-between flex-wrap">
+            <div className="export_csv">
+              <Button className="export_btn">Export To Csv</Button>
+            </div>
+            <div className="filter_gender">
+              <div className="filter">
+                <h3>Filter By Gender</h3>
+                <div className="gender d-flex justify-content-between">
+                  <Form.Check
+                    type={"radio"}
+                    label={`All`}
+                    name="gender"
+                    value={"All"}
+                    defaultChecked
+                  />
+                  <Form.Check
+                    type={"radio"}
+                    label={`Male`}
+                    name="gender"
+                    value={"Male"}
+                  />
+                  <Form.Check
+                    type={"radio"}
+                    label={`Female`}
+                    name="gender"
+                    value={"Female"}
+                  />
+                </div>
+              </div>
+            </div>
+            {/* short by value  */}
+            <div className="filter_newold">
+              <h3>Shorts By Value</h3>
+              <Dropdown>
+                <Dropdown.Toggle className="dropdown_btn" id="dropdown-basic">
+                  <i class="fa-solid fa-sort"></i>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item>New</Dropdown.Item>
+                  <Dropdown.Item>Old</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+
+            {/* filter by status  */}
+
+            <div className="filter_status">
+              <div className="status">
+                <h3>Filter By status</h3>
+                <div className="status_radio d-flex justify-content-around flex-wrap">
+                  <Form.Check
+                    type={"radio"}
+                    label={`All`}
+                    name="status"
+                    value={"All"}
+                    defaultChecked
+                  />
+                  <Form.Check
+                    type={"radio"}
+                    label={`Active`}
+                    name="status"
+                    value={"Active"}
+                  />
+                  <Form.Check
+                    type={"radio"}
+                    label={`InActive`}
+                    name="status"
+                    value={"InActive"}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
