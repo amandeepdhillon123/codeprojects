de6ece800b8c704e618fe44f4085d73d4eac0612
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-08 16:29:11 +0530

    authentication and autorization

--- auth/routes/rout.js
+++ auth/routes/rout.js
@@ -4,9 +4,32 @@ const express = require("express");
 
 const router = express.Router();
 
-const {createSign}= require("../controller/auth");
+const {createSign,login}= require("../controller/auth");
+const{auth,isStudent,isAdmin}= require("../middleware/middle")
 
 
 router.post('/sign',createSign)
+router.post('/login',login)
+
+router.get("/test", auth, (req,res) =>{
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for TESTS',
+    });
+});
+
+router.get("/student", auth, isStudent, (req,res) => {
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for Students',
+    });
+} );
+
+router.get("/admin", auth, isAdmin, (req,res) => {
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for Admin',
+    });
+});
 
 module.exports = router
