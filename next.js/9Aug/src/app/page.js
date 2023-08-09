c8748d97b71b2f3ec1aa01c0f99281152ce0c830
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-09 14:23:42 +0530

    conditonal styels

--- next.js/9Aug/src/app/page.js
+++ next.js/9Aug/src/app/page.js
@@ -1,11 +1,18 @@
-import "./style.css"
-import Shyam from '@/../style/ram.module.css'
-import ram from "./page.module.css"
-export default function Home() {
+'use client'
+import React from 'react'
+import { useState } from 'react'
+import style from './style.module.css'
+const page = () => {
+
+  const[color,setCol] = useState('red')
   return (
-    <main>
-      <h1 className={ram.main}>hello ji</h1>
-      <h2 className={Shyam.main}>hii name</h2>
-    </main>
-  );
+    <div>
+      <h1 className={ color == 'red' ? style.red : style.green}>condtiton with style</h1>
+        <h2 style={{backgroundColor:color==='red'? 'red':'green'}}>heading 2</h2> 
+        
+       <button onClick ={()=> setCol("green")}>update color</button>
+    </div>
+  )
 }
+
+export default page
