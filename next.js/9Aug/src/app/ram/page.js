ce47d17c8c4db63e7da12d2f32bac5aab93594d4
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-10 21:41:26 +0530

    metatag

---
+++ next.js/9Aug/src/app/ram/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const  Ram = () => {
+  return (
+    <div>Ram</div>
+  )
+}
+
+export default  Ram
+
+
+export function generateMetadata({params}){
+    return{
+        title:"ramfal"
+    }
+}
