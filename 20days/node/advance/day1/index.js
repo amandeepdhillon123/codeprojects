6a6c7bf08cc6f14757fad4dfe9ba384ea6c206d3
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-12 16:53:40 +0530

    nde core modules

---
+++ 20days/node/advance/day1/index.js
@@ -0,0 +1,13 @@
+const fs = require("fs")
+console.log(fs)
+
+const http= require("http")
+
+console.log(http)
+
+const path = require("path")
+console.log(path)
+
+console.log(__dirname)
+console.log("+++++++++++++++++++++++++++++++++++")
+console.log(__filename)
