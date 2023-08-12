14b4f29ac6271f19e864232353240ccc3a1f9464
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-12 16:16:02 +0530

    readline module in nodejs

---
+++ 20days/node/pro/index.js
@@ -0,0 +1,18 @@
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+
+rl.question("Write your name :",(name)=>{
+    console.log("your name is :" + name)
+    rl.close();
+})
+
+rl.on('close',()=>{
+    console.log("interface is closed");
+    process.exit(0);
+
+})
