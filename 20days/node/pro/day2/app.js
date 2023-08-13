3c67136d0bd23adf4ea09a5470503c054ff02c01
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-13 12:20:32 +0530

    wirking with json data

--- 20days/node/pro/day2/app.js
+++ 20days/node/pro/day2/app.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 
 const http = require("http");
 
+let ram = JSON.parse(fs.readFileSync("./data/data,.json","utf-8"))
+
 const address = fs.readFileSync("./index.html","utf-8");
 console.log(address)
 
@@ -27,7 +29,26 @@ const server = http.createServer((req,resp)=>{
              "my-Header" :" hello world"
           })
         resp.end(address.replace('{{%CONTENT%}}','this is about page'))
-    }else{
+    }
+
+    else if( path.toLocaleLowerCase() === "/products")
+    {
+        resp.writeHead(200,{
+            "Content-Type" :"application/json",
+             "my-Header" :" hello world"
+          })
+
+        //   fs.readFile("./data/data,.json","utf-8",(err,data)=>{
+        //     let product =JSON.parse(data)
+        //     resp.end(data)
+        //     console.log(product)
+        //   })
+
+        resp.end("you are in product page")
+        console.log(ram)
+     
+    }
+    else{
         resp.writeHead(404,{
        
              "my-Header" :" hello world"
@@ -38,6 +59,6 @@ const server = http.createServer((req,resp)=>{
  
 }) 
 
-server.listen(6010,"127.0.0.1",()=>{
+server.listen(6013,"127.0.0.1",()=>{
     console.log("server chl gya bhai");
 })
