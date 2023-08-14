6fd70572a93efc9aa2004c82074768e003e5771b
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-14 10:10:59 +0530

    working with josn data

--- 20days/node/pro/day3/index.js
+++ 20days/node/pro/day3/index.js
@@ -2,6 +2,11 @@
 const fs = require("fs");
 const http = require("http")
 const html = fs.readFileSync("./temp/index.html","utf-8")
+let productList =fs.readFileSync('./data/products.json',"utf-8")
+
+let jsObject =JSON.parse(productList)
+console.log(jsObject)
+
 
 const server = http.createServer((req,resp)=>{
      const path = req.url
@@ -22,6 +27,20 @@ const server = http.createServer((req,resp)=>{
             "My-Header":"custom headers"
         })
          resp.end(html.replace("{{%content%}}","this is about page"))
+     }
+     else if(path.toLocaleLowerCase() ==="/products")
+     {
+        resp.writeHead(200,{
+            'Content-Type':'application/json',
+            "My-Header":"custom headers"
+        })
+        //  fs.readFile("./data/products.json","utf-8",(err,data)=>{
+        //      let storeData =JSON.parse(data)
+        //      console.log(storeData)
+        //     resp.end(data)
+        //  })
+
+        resp.end(productList)
      }
      else{
         resp.writeHead(404,{
