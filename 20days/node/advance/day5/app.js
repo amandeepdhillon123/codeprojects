3313f2a6299dc5da7ed4f25163f53850abfbb32c
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-19 11:03:08 +0530

    parsinf query

--- 20days/node/advance/day5/app.js
+++ 20days/node/advance/day5/app.js
@@ -2,7 +2,9 @@
 const fs= require("fs");
 const http = require("http");
 const html= fs.readFileSync('./file/index.html',"utf-8")
-const PORT = 4000
+const PORT = 5500
+const url =require("url")
+// console.log(url)
 
 let products = JSON.parse(fs.readFileSync("./data/product.json","utf-8"))
 // console.log(products)
@@ -11,17 +13,21 @@ let productList = fs.readFileSync("./file/productList.html","utf-8")
  
 
 let productHtml =products.map((ele)=>{
-    let output = productList.replace("{{%id%}}",ele.id);
-    output =output.replace("{{%name%}}",ele.name)
+    let output = productList.replace("{{%name%}}",ele.name)
+    
     output =output.replace("{{%username%}}",ele.username)
     output =output.replace("{{%email%}}",ele.email)
     output =output.replace("{{%phone%}}",ele.phone)
     output =output.replace("{{%website%}}",ele.website)
+    output =output.replace("{{%ID%}}",ele.id)
     return output
 })
 
 http.createServer((req,resp)=>{
-           path =req.url;
+
+        let {query,pathname:path} = url.parse(req.url,true)
+        console.log(query.id,path)
+        //    path =req.url;
 
      if(path ==='/' || path ==='/home')
      {
@@ -29,14 +35,21 @@ http.createServer((req,resp)=>{
      }
      else if(path === "/products")
      {
-
+        if(!query.id)
+        {
+           
         let response =html.replace("{{%content%}}",productHtml.join(","))
         resp.writeHead(200,{
             'Content-Type':'text/html'
         })
 
         resp.end(response)
-        console.log(response)
+        }
+        else{
+             resp.end(query.id)
+        }
+
+        // console.log(response)
         // console.log(productHtml.join(","))
      }
      else
