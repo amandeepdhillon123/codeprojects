60a87f64b43276a2404cd0c4c8097db0b99f062a
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-26 21:07:28 +0530

    some change s

--- auth/routes/rout.js
+++ auth/routes/rout.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 
-const router = express.Router();
+const router =  express.Router();
 
 const {createSign,login}= require("../controller/auth");
 const{auth,isStudent,isAdmin}= require("../middleware/middle")
