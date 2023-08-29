f30d36a36f1ee66f9876444812409254f44ad2f1
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-29 07:20:26 +0530

    throew

--- interviews/js/js2/Error/index.js
+++ interviews/js/js2/Error/index.js
@@ -3,7 +3,7 @@
 //   console.log('An error occurred:', error.message);
 // // 
 
-}
+// }
 //finally block to execute code regardless of whether
 //an error occured or not
 // finally{
@@ -13,3 +13,14 @@
 //An error occurred: someUndefinedVariable is not defined
 //finally executed
 // 
+
+// function validateUserAge(age) {
+//       if (typeof age !== "number") {
+//         throw new Error("Age must be a number");
+//       }
+//       if (age < 18) {
+//         throw new Error("User must be at least 18 years old");
+//       }
+//       console.log("User age is valid");
+//     }
+    
