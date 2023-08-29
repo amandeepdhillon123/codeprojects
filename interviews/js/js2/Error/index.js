32c14135d525178312edb282dabfca91b5fc2469
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-29 07:19:47 +0530

    finally block

--- interviews/js/js2/Error/index.js
+++ interviews/js/js2/Error/index.js
@@ -2,3 +2,14 @@
 //   console.log(result);
 //   console.log('An error occurred:', error.message);
 // // 
+
+}
+//finally block to execute code regardless of whether
+//an error occured or not
+// finally{
+//   console.log("finally executed");
+// }
+// //Output
+//An error occurred: someUndefinedVariable is not defined
+//finally executed
+// 
