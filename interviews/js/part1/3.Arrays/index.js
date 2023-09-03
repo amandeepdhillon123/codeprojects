c650e471a65eac79cb0978b7d0bbdb000d0f2585
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-03 08:56:43 +0530

    find and filter

--- interviews/js/part1/3.Arrays/index.js
+++ interviews/js/part1/3.Arrays/index.js
@@ -8,6 +8,21 @@
 
 // // }))
 
+// const array = [1, 2, 3, 4, 5];
+// let a = array.indexOf(3);
+// console.log(a); 
+
+
 const array = [1, 2, 3, 4, 5];
-let a = array.indexOf(3);
-console.log(a); 
+
+// let c= array.find((num)=>
+//     num % 2 === 0
+   
+// )
+// console.log(c)
+
+let d= array.filter((num) =>{
+    return num % 2 ===0
+})
+
+console.log(d)
