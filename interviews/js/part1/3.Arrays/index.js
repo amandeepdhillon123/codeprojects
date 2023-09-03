377ca953e8cf268528419e722ed38d1fe42b8e7e
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-03 08:52:31 +0530

    indexof

--- interviews/js/part1/3.Arrays/index.js
+++ interviews/js/part1/3.Arrays/index.js
@@ -1,9 +1,13 @@
-const name =[1,2,3]
-name.push("shyma")
-name.unshift("shyma")
-name.pop("shyma")
-name.shift("shyma")
-// console.log(name)
-// console.log(name.filter((ele)=>{
+// const name =[1,2,3]
+// name.push("shyma")
+// name.unshift("shyma")
+// name.pop("shyma")
+// name.shift("shyma")
+// // console.log(name)
+// // console.log(name.filter((ele)=>{
 
-// }))
+// // }))
+
+const array = [1, 2, 3, 4, 5];
+let a = array.indexOf(3);
+console.log(a); 
