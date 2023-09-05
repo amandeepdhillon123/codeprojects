1add1b291b8b6f34f2a084d1d88602edeb202eb0
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-05 09:47:01 +0530

    conditon applu

--- interviews/js/part1/4.loops/index.js
+++ interviews/js/part1/4.loops/index.js
@@ -4,8 +4,13 @@
 //     }
     
 
-    let j = 0;
-while (j < 5) {
-  console.log(j);
-  j++;
+//     let j = 0;
+// while (j < 5) {
+//   console.log(j);
+//   j++;
+// }
+
+while ("a" == "a") {
+      console.log("Happy");  
 }
+
