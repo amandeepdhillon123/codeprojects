7ecd6173c6d8d5a495b1be1d942b3c2b1813d95a
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-05 09:46:14 +0530

    while loop

--- interviews/js/part1/4.loops/index.js
+++ interviews/js/part1/4.loops/index.js
@@ -1,5 +1,11 @@
 //for-loop
-for (let i = 0; i < 5; i++) {
-     console.log(i);
-    }
+// for (let i = 0; i < 5; i++) {
+//      console.log(i);
+//     }
     
+
+    let j = 0;
+while (j < 5) {
+  console.log(j);
+  j++;
+}
