9bc7f7b2cc6bb0fd422338045f9985b45b75b616
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-05 09:50:26 +0530

    do while lopp

--- interviews/js/part1/4.loops/index.js
+++ interviews/js/part1/4.loops/index.js
@@ -10,7 +10,21 @@
 //   j++;
 // }
 
-while ("a" == "a") {
-      console.log("Happy");  
-}
+// while ("a" == "a") {
+//       console.log("Happy");  
+// }
+
+// let j = 0;
+// while (j < 5) {
+// console.log(j);
+// j++;
+// }
+
+// let k = 0;
+// do{
+// console.log(k);
+// k++;
+// }
+// while (k);
+
 
