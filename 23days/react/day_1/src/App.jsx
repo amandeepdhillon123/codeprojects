5290e1db50ecd9a5b35289a9d18fa16dfad616ad
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-07 07:47:07 +0530

    App.js

--- 23days/react/day_1/src/App.jsx
+++ 23days/react/day_1/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import {Fun} from "./components/Fun"
 import Class from './components/Class'
+import JS from './components/Js'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -12,6 +13,7 @@ function App() {
      <h1>hello</h1>
      <Class/>
      <Fun/>
+     <JS/>
      </>
   )
 }
---
+++ 23days/react/day_1/src/components/Js.jsx
@@ -0,0 +1,9 @@
+
+import React from "react";
+
+const JS =()=>{
+    
+    return React.createElement("h1",null,"hello ji")
+}
+
+export default JS;
