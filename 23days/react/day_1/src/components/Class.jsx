a461b08a902ecd585fceced6a460405504229670
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-07 07:44:18 +0530

    components

---
+++ 23days/react/day_1/src/components/Class.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Class extends Component {
+    render()
+    {
+        return(
+            <>
+             <h1>class components</h1>
+          
+            </>
+        )
+    }
+}
+export default Class;
