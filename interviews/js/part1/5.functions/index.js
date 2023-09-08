5347165fe29072d15ea11add07098e8d8ca98f5a
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-08 10:32:38 +0530

    anonymous functin

--- interviews/js/part1/5.functions/index.js
+++ interviews/js/part1/5.functions/index.js
@@ -1,6 +1,9 @@
-function sum(a, b) {
-    let c = a + b;
-    console.log(c);
-    }
-    sum(1,2)
+// function sum(a, b) {
+//     let c = a + b;
+//     console.log(c);
+//     }
+//     sum(1,2)
 
+    console.log(function(a, b) {
+    return a * b}(4, 5)); 
+        
