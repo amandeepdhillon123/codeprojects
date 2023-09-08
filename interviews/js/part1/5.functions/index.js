847964629551f58b0cb0731c9a08f0a3344e81cc
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-08 10:42:14 +0530

    callback

--- interviews/js/part1/5.functions/index.js
+++ interviews/js/part1/5.functions/index.js
@@ -29,3 +29,14 @@
 
 // const add =(x,y)=> x+ y;
 // console.log(add(5,6))
+
+// function add(x,y){
+//     return x * y
+// }
+
+// function display(operation){
+    
+//     console.log(operation);
+// }
+// display(add(5,4))
+    
