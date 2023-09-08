14ed7a64768ca9dc0f2b4daf9f73b3362ee3d203
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-08 10:34:28 +0530

    function expressino

--- interviews/js/part1/5.functions/index.js
+++ interviews/js/part1/5.functions/index.js
@@ -4,6 +4,21 @@
 //     }
 //     sum(1,2)
 
-    console.log(function(a, b) {
-    return a * b}(4, 5)); 
+    // console.log(function(a, b) {
+    // return a * b}(4, 5)); 
         
+
+    // Anonymous function expression 
+// const add = function(a, b) {
+//       return a + b;
+//     };
+//     console.log(add(5, 3)); 
+//     // Output: 8
+//     // Named function expression 
+//     const addNamed = function sum(a, b) {
+//       return a + b;
+//     };
+//     console.log(addNamed(5, 3)); 
+//     // Output: 8
+//     
+    
