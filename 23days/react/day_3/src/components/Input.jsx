c87d94bfdec92f291379bc30771992e1af8d4ad2
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-10 07:19:27 +0530

    input

---
+++ 23days/react/day_3/src/components/Input.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const Input = () => {
+  const [data,setData]= useState("get data from input")
+  const [print,printData] = useState(null)
+  
+   function getData(e){
+        console.log(e.target.value)
+        setData(e.target.value)
+        printData("")
+    }   
+  return (
+    <div>
+         <h1>{print ? <h1> {data }</h1>: null}</h1>
+        <input type="text" onChange={getData} />
+      <button onClick={printData}>print</button>
+    </div>
+  )
+}
+
+export default Input
