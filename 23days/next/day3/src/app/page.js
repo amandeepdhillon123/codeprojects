72e4df0b92ab555fac67f90d60390784ce0fa693
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-10 09:20:07 +0530

    file base routing

---
+++ 23days/next/day3/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+// import Page from './compnent/about/page'
+export default function Home() {
+  return (
+    <>
+  <h1>hello</h1>
+    </>
+  )
+}
