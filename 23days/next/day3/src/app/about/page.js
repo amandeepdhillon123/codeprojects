19f2a5f46256f61819c36c9e3c5c04ba3f63d3a3
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-10 10:06:59 +0530

    login

--- 23days/next/day3/src/app/about/page.js
+++ 23days/next/day3/src/app/about/page.js
@@ -1,9 +1,13 @@
-
-
+"use client"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
 const page = () => {
+    const router = useRouter()
   return (
     <div>
         <h1>page</h1>
+        <Link href="/">Home  page</Link> <br /> <br />
+        <button onClick={()=> router.push('/')}>GO to home page</button>
     </div>
   )
 }
