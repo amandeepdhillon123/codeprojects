6f0f296240dbdc5901be346c1bd7b7622cad9229
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-13 06:46:17 +0530

    next js css

--- 23days/next/day_5/src/app/page.js
+++ 23days/next/day_5/src/app/page.js
@@ -1,9 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
+import road from './custom.module.css'
+import outside from '@/style/outside.module.css'
 export default function Home() {
   return (
     <>
-    <h1>hello jii</h1></>
+    <h1 className='main'>hello jii</h1>
+     <h1 className='main'>hello jii</h1>
+      <p className={ outside.main}>some text</p>
+      <h1 className={road.main}>css module </h1>
+     </>
   )
 }
