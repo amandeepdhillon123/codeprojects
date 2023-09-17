577a4561d67e0dd75111a1bf66c3328b7c9e2713
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-17 09:56:38 +0530

    registeration completed

--- 23days/projects/Pagination/client/src/pages/Register/Register.jsx
+++ 23days/projects/Pagination/client/src/pages/Register/Register.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import "./register.css";
 import Card from "react-bootstrap/Card";
 // for form
@@ -7,15 +8,125 @@ import Form from "react-bootstrap/Form";
 import  Row  from "react-bootstrap/Row";
 // selct tag
 import Select from 'react-select';
+// toastify 
+import { ToastContainer,toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
+
+
+
 
 const Register = () => {
+
+  //  getdata input 
+
+  const[inputdata,setInputData] =useState({
+    fname:"",
+    lname:"",
+    email:"",
+    mobile:"",
+    gender:"",
+    location:""
+  })
+
+  // for status 
+  const [status, setStatus] = useState("Active");
+  const[image,setImage] =useState("")
+  const[preview,setPreview]=useState()
 
     // react select 
     const options = [
       { value: 'Active', label: 'Active' },
-      { value: 'InActive', label: 'InActive' },
- 
-    ];
+      { value: 'InActive', label: 'InActive' } ];
+
+      // setInput values 
+      const setInputValue= (e)=>{
+       ;
+        const {name,value} =e.target;
+        setInputData({...inputdata,[name]:value})
+   
+      }
+
+      // console.log(inputdata)
+
+      // setsatatus 
+
+      const setStatusValue =(e)=>{
+          setStatus(e.value)
+
+          // console.log(e.value)
+      }
+
+      // profile set 
+
+      const setProfile =(e)=>{
+        setImage(e.target.files[0])
+
+      }
+
+      //  submit user data
+        const submitUserData=(e)=>{
+          e.preventDefault();
+
+          const {fname,lname, email,mobile, gender,location} = inputdata
+            
+               if(fname === "")
+                {
+                  toast.error("First name is Required")
+                }
+                else if(lname === "")
+                {
+                  toast.error("last name is Required")
+                }
+            
+                else if(email === "")
+                {
+                  toast.error("Email  is Required")
+                }
+                else if(!email.includes === "@")
+                {
+                  toast.error("Enter Valid Email")
+                }
+
+                else if(mobile === ""){
+                    toast.error("Mobile is Required")
+                }
+                else if(mobile.length > 10){
+                  toast.error("Enter Valoid Mobile")
+              }
+              else if(gender === ""){
+                toast.error("Gender is required")
+              }
+              else if( status === ""){
+                toast.error(" Status is required")
+              }
+              else if(image === ""){
+                toast.error("Profile is required")
+              }
+              else if(location === ""){
+                toast.error("location is required")
+              }
+              else{
+                toast.success("Registeration successful done !")
+              }
+                
+        }
+
+
+      // preview image 
+
+      useEffect(()=>{
+            if(image){
+              setPreview(URL.createObjectURL(image))
+            }
+      },[image])
+      
+
+     
+
+
+
+      
+
   
   return (
     <>
@@ -24,8 +135,7 @@ const Register = () => {
         <Card className="shadow mt-3 p-3">
           <div className="profile_div text-center">
             <img
-              src="https://w0.peakpx.com/wallpaper/639/974/HD-wallpaper-bhagat-singh-bhagatsingh-dom-domfighter-domwarrior-warrior-thumbnail.jpg"
-              alt="bhagat_Singh_Image"
+                 src={ preview ? preview :"https://w0.peakpx.com/wallpaper/639/974/HD-wallpaper-bhagat-singh-bhagatsingh-dom-domfighter-domwarrior-warrior-thumbnail.jpg"} alt=""
             />
           </div>
           <Form>
@@ -33,13 +143,13 @@ const Register = () => {
                {/* firstname  */}
             <Form.Group className="mb-3 col-lg-6"  controlId="formBasicEmail">
               <Form.Label>FirstName</Form.Label>
-              <Form.Control type="text" placeholder="Enter FirstName" name="fname" />
+              <Form.Control type="text" placeholder="Enter FirstName" name="fname" onChange={setInputValue} />
               
             </Form.Group>
                {/* last name  */}
             <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
               <Form.Label>LastName</Form.Label>
-              <Form.Control type="text" placeholder="Enter LastName" name="lname" />
+              <Form.Control type="text" placeholder="Enter LastName" name="lname" onChange={setInputValue} />
               
             </Form.Group>
 
@@ -47,13 +157,13 @@ const Register = () => {
 
             <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter Email" name="email" />
+              <Form.Control type="email" placeholder="Enter Email" name="email"  onChange={setInputValue}/>
               
             </Form.Group>
           {/*  mobile Number */}
             <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
               <Form.Label>Mobile</Form.Label>
-              <Form.Control type="text" placeholder="Enter Mobile" name="lname" />
+              <Form.Control type="text" placeholder="Enter Mobile" name="mobile" onChange={setInputValue}/>
               
             </Form.Group>
 
@@ -67,6 +177,7 @@ const Register = () => {
                 label={'Male'}
                 name="gender"
                 value={"Male"}
+                onChange={setInputValue}
                 />
 
 <Form.Check 
@@ -74,22 +185,23 @@ const Register = () => {
                 label={'Female'}
                 name="gender"
                 value={"Female"}
+                onChange={setInputValue}
                 />
               
             </Form.Group>
 
-            {/* select tag */}
+            {/* mobile */}
 
             <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-              <Form.Label>Mobile</Form.Label>
-              <Select options={options}/>
+              <Form.Label>Select Your Status</Form.Label>
+              <Select options={options} onChange={setStatusValue}/>
               
             </Form.Group>
             {/* select your profile */}
 
             <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
               <Form.Label>Slect Your Profile</Form.Label>
-              <Form.Control type="file" placeholder="Select Your Profile" name="user_profile" />
+              <Form.Control type="file" placeholder="Select Your Profile" name="user_profile" onChange={setProfile} />
               
             </Form.Group>
 
@@ -97,7 +209,7 @@ const Register = () => {
 
             <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
               <Form.Label>Enter Your Location</Form.Label>
-              <Form.Control type="text" placeholder="Enter Your Location" name="location" />
+              <Form.Control type="text" placeholder="Enter Your Location" name="location"  onChange={setInputValue}/>
               
             </Form.Group>
 
@@ -106,13 +218,14 @@ const Register = () => {
             
 
             
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" onClick={submitUserData}>
               Submit
             </Button>
             </Row>
            
           </Form>
         </Card>
+        <ToastContainer position="top-right" theme="light"/>
       </div>
     </>
   );
