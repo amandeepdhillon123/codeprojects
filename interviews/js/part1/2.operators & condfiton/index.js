99a285114a7573dc76b8845c87f3a3268eb54266
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-19 08:02:11 +0530

    assignment

--- interviews/js/part1/2.operators & condfiton/index.js
+++ interviews/js/part1/2.operators & condfiton/index.js
@@ -1,11 +1,18 @@
 
 // artithmetic operator 
-let x= 5;
-let y= 3;
+// let x= 5;
+// let y= 3;
 
-console.log(x+y);
-console.log(x-y);
-console.log(x*y);
-console.log(x/y);
-console.log(x%y);
-console.log(x**y);
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+// console.log(x**y);
+
+// assignment 
+
+let x= 4;
+
+x += 5;
+console.log(x)
