81b5ab08a9750c05510452a6b856f32b83866f2c
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-19 08:18:42 +0530

    condtions

--- interviews/js/part1/2.operators & condfiton/index.js
+++ interviews/js/part1/2.operators & condfiton/index.js
@@ -48,5 +48,12 @@
 // let result = conditon ? "yes": "no" ;
 // console.log(result)
 
-let result = true &&  func();
-console.log(result)
+// let result = true &&  func();
+// console.log(result)
+
+let x = 5;
+if (x > 10) { console.log("1");
+} else if (x < 5) { console.log("2");
+} else { console.log("3");
+}
+
