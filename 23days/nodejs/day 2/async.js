b19d4fa07b241c5292083feeb3b8f7c0aef844b0
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-20 08:47:37 +0530

    ream.txt

--- 23days/nodejs/day 2/async.js
+++ 23days/nodejs/day 2/async.js
@@ -7,17 +7,41 @@
 // },2000)
 // console.log(a+b)let
 
-let a= 10;
-let b=0;
+// let a= 10;
+// let b=0;
 
-let waitingData =new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve(30)
-    },2000)
-})
+// let waitingData =new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve(30)
+//     },2000)
+// })
 
 
-waitingData.then((data)=>{
-    b = data;
-    console.log(a + b)
-})
+// waitingData.then((data)=>{
+//     b = data;
+//     console.log(a + b)
+// })
+  let a= 20;
+  let b= 0
+  let waitingData= async function show(){
+    try {
+         let b= 20;
+         let sum = (a + b)
+         console.log(sum)
+    } catch (error) {
+        console.log(error)
+    }
+  }
+ 
+waitingData()
+
+
+
+
+
+
+
+
+
+
+
