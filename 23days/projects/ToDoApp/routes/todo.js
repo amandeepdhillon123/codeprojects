b6b4a112b5eabbb8ad9ed3c6f68ebeca53d88e9d
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-22 21:14:52 +0530

    todo js

--- 23days/projects/ToDoApp/routes/todo.js
+++ 23days/projects/ToDoApp/routes/todo.js
@@ -6,11 +6,13 @@
   const {createTodo} = require("../controllers/createTodo");
   const {getTodo,getTodoById} = require("../controllers/getTodo")
   const {updateTodo}= require("../controllers/update")
-
+  const {deleteTodo} = require("../controllers/deleteTodo")
+ 
   router.post("/createTodo",createTodo)
   router.get("/getTodos", getTodo);
   router.get("/getTodos/:id", getTodoById);
   router.put("/updateTodo/:id", updateTodo);
+  router.delete('/deleteTodo/:id',deleteTodo)
   
 
   module.exports =router;
