706ae4678d8ec52eaa74656bcf6dd31257f8ca45
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-26 22:57:07 +0530

    som change s

---
+++ 23days/projects/MERNAuth/backend/index.js
@@ -0,0 +1,23 @@
+
+require("dotenv").config()
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const router =require("./routes/route")
+const dbConnect= require("./config/database")
+const cors = require("cors")
+const PORT = process.env.PORT || 8009
+
+
+app.use(express.json())
+app.use(cookieParser());
+app.use(cors())
+
+app.use(router)
+ 
+
+dbConnect();
+
+app.listen(PORT,()=>{
+   console.log(`server starts at PORT ${PORT}`)
+})
