a041509f0b287f7e86389b2f21840781c7c813a6
amandeepdhillon123 <amandeep24397@gmail.com>
2023-10-19 07:28:35 +0530

    varibales

---
+++ interviews/js/part1/1. variables/index.js
@@ -0,0 +1,33 @@
+// 
+// var 
+
+// function example(){
+//     var name ="amandeep"
+//     console.log(name)
+// }
+// // console.log(name)
+// example()
+
+// if(true){
+//     var nam ="amandeep"
+// }
+
+// console.log(nam)
+
+// var name ="amandeep"
+//  var name ="raj"
+// //  console.log(name)
+
+// let name = "rama";
+//   name ="raj"
+//   console.log(name)
+
+//   let name ="amandeep"
+//   function exa(){
+
+//     // let name ="raj"
+//     console.log(name)
+//   }
+//   exa()
+
+// let name;
