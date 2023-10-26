67215e5636914360ba0c12fe1f424403f5262d15
amandeepdhillon123 <amandeep24397@gmail.com>
2023-10-26 06:46:35 +0530

    todo app

--- amariya/backend/TodoApp/models/todo.js
+++ amariya/backend/TodoApp/models/todo.js
@@ -2,8 +2,25 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name:{
+    title:{
         type:String,
         required:true
+    },
+    description:{
+        tyoe:String,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        required:true,
+        default:Date.now()
+
+    },
+    updatedAt:{
+        type:Date,
+        required:true,
+        default:Date.now()
     }
 })
+
+module.exports = mongoose.model("users",userSchema)
