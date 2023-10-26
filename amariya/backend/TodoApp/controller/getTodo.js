aad7102a0cf28f2bb3f357c6cd70c9d55ab6371d
amandeepdhillon123 <amandeep24397@gmail.com>
2023-10-26 07:03:23 +0530

    todo app

--- amariya/backend/TodoApp/controller/getTodo.js
+++ amariya/backend/TodoApp/controller/getTodo.js
@@ -9,15 +9,36 @@ exports.getTodo =async(req,resp)=>{
         resp.status(200).json({
             success:true,
             data:response,
-            required:true
+            message:"get data successful"
         })
         
     } catch (error) {
         
+        resp.status(500).json({
+            success:false,
+            message:"internal server error",
+            error:error.message
+        })
+    }
+}
+
+
+exports.getSingle =async(req,resp)=>{
+    try {
+        const{id} = req.params;
+
+        const todo = await User.findById({_id:id})
+
         resp.status(200).json({
             success:true,
             data:response,
-            required:true
+            message:"get data successful"
+        })
+    } catch (error) {
+        resp.status(500).json({
+            success:false,
+            message:"internal server error",
+            error:error.message
         })
     }
 }
