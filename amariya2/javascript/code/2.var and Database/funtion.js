41a76d36ef42179e97e0e6e7cf915f0d95da6f91
amandeepdhillon123 <amandeep24397@gmail.com>
2023-10-29 21:32:13 +0530

    expression

--- amariya2/javascript/code/2.var and Database/funtion.js
+++ amariya2/javascript/code/2.var and Database/funtion.js
@@ -1,11 +1,24 @@
 // name function 
-function sum(a,b){
-  return a + b;
-};
-console.log(sum(5,3))
+// function sum(a,b){
+//   return a + b;
+// };
+// console.log(sum(5,3))
 
 // anonymous 
 
-console.log(function(a, b){
-    return a* b;
-}(4,5))
+// console.log(function(a, b){
+//     return a* b;
+// }(4,5))
+
+// expression
+
+// const add = function(a,b){
+//     return a + b;
+// };
+// console.log(add(5,3))
+
+const add = function sum(a,b){
+    return a + b
+}
+
+console.log(add(5,3));
