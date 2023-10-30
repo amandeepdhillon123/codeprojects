da3f20f885955332bb7f1a28a9926c363ddaa8fd
amandeepdhillon123 <amandeep24397@gmail.com>
2023-10-30 21:11:56 +0530

    do wilw lopp

--- amariya2/javascript/code/2.var and Database/loop.js
+++ amariya2/javascript/code/2.var and Database/loop.js
@@ -2,9 +2,23 @@
 //     console.log(i)
 // }
 
-var a=0;
+// var a=0;
 
-while(a<5){
-    console.log("amaneep")
+// while(a<5){
+//     console.log("amaneep")
+//     a++;
+// }
+
+// let k = 0;
+// do{
+//   console.log(k);
+//   k++;
+// }
+// while (k > 1);
+
+let a = 0 ;
+do{
+    console.log(a)
     a++;
 }
+while(a > 1)
