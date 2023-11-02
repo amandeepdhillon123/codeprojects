0605e34595ccba22365c45170fc2e4831d41b537
amandeepdhillon123 <amandeep24397@gmail.com>
2023-11-02 11:46:15 +0530

    pop and shift

--- amariya2/javascript/code/2.var and Database/array.js
+++ amariya2/javascript/code/2.var and Database/array.js
@@ -31,10 +31,17 @@
 
 // console.log(array)
 
-let arr1=[5,6];
+// let arr1=[5,6];
 
-let arr2 = [7,8];
+// let arr2 = [7,8];
 
-let array3 =arr1.concat(arr2)
+// let array3 =arr1.concat(arr2)
 
-console.log(array3)
+// console.log(array3)
+
+
+let arr1 =[1,2,3,4,5];
+
+let pop =arr1.pop();
+
+console.log(pop)
