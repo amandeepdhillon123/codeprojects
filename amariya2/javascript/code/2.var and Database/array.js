22b2bf2dc3e3836445e649ad5fb714bfa558f7fc
amandeepdhillon123 <amandeep24397@gmail.com>
2023-11-02 11:37:57 +0530

    get element

---
+++ amariya2/javascript/code/2.var and Database/array.js
@@ -0,0 +1,20 @@
+// const array =[1,2,3,4,5];
+
+// get 
+// console.log(arry.indexOf(1))
+
+//  const c = array.find((num) => {
+//    return num % 2 === 0
+//  })
+
+//  console.log(c)
+
+const array =[1,2,3,4,5];
+
+let d = array.filter((num) =>{
+    return num % 2 === 0;
+
+    
+})
+
+console.log(d)
