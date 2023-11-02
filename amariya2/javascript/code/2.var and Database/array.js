8cf6f9b349959184716021ae441436efb5b95602
amandeepdhillon123 <amandeep24397@gmail.com>
2023-11-02 11:40:54 +0530

    slice

--- amariya2/javascript/code/2.var and Database/array.js
+++ amariya2/javascript/code/2.var and Database/array.js
@@ -9,12 +9,17 @@
 
 //  console.log(c)
 
-const array =[1,2,3,4,5];
+// const array =[1,2,3,4,5];
 
-let d = array.filter((num) =>{
-    return num % 2 === 0;
+// let d = array.filter((num) =>{
+//     return num % 2 === 0;
 
     
-})
+// })
 
-console.log(d)
+// console.log(d)
+
+const array =["a","b","c","d"];
+
+let e =array.slice(1,4);
+console.log(e)
