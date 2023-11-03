d1d594fc0a0eb7a41472f31391d8aa266ade046a
amandeepdhillon123 <amandeep24397@gmail.com>
2023-11-03 08:50:06 +0530

    database connection

--- amariya/Fashion/server/app.js
+++ amariya/Fashion/server/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app= express();
 const cors = require("cors")
 const PORT = 4009;
+const dbConnect = require("./db/conn")
 
 app.use(express.json());
 app.use(cors())
@@ -11,6 +12,10 @@ app.use(cors())
 app.get("/",(req,resp)=>{
     resp.status(200).json("server starts")
 })
+
+// data base connection 
+
+// dbConnect()
 
 app.listen(PORT,()=>{
     console.log(`server starts at ${PORT}`)
---
+++ amariya/Fashion/server/model/admin/adminModel.js
@@ -0,0 +1,42 @@
+        const mongoose = require("mongoose");
+        const validator = require("validator");
+        const adminSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("not valid email");
+            }
+            },
+        },
+        profile: {
+            type: String,
+            required: true,
+        },
+        mobile: {
+            type: String,
+            required: true,
+            minlength: 10,
+            maxlength: 10,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        tokens: [
+            {
+            token: {
+                type: String,
+                required: true,
+            },
+            },
+        ],
+        });
+
+        module.exports = mongoose.model("admins", adminSchema);
