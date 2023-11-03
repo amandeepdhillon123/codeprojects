6e2eadb19df941e75fdc54270851ad281f9591ec
amandeepdhillon123 <amandeep24397@gmail.com>
2023-11-03 10:46:21 +0530

    comparison operatro

--- amariya2/javascript/code/2.var and Database/operator.js
+++ amariya2/javascript/code/2.var and Database/operator.js
@@ -13,7 +13,24 @@
 // console.log( x ** y);
 
 // assignment operator 
-let x= 10;
-x = x + 5;
-x *=5
-console.log(x)
+// let x= 10;
+// x = x + 5;
+// x *=5
+// console.log(x)
+
+
+// conparision operatro 
+
+let x =5;
+let y= 3;
+// console.log(x > y)
+
+// console.log(x < y)
+// console.log(x >= y)
+// console.log(x <= y)
+// console.log(x == y)
+// console.log(x === y)
+// console.log(x !== y)
+// console.log(x !== y)
+
+
