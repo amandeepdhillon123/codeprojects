7f1e26831775bc1ad739782dae7920159d5d28e7
amandeepdhillon123 <amandeep24397@gmail.com>
2023-11-04 10:18:43 +0530

    primitive and non-primitive

--- amariya2/javascript/code/2.var and Database/data.js
+++ amariya2/javascript/code/2.var and Database/data.js
@@ -1,16 +1,34 @@
+// //  string
+// let message =" hello"
 
-//  string
-let message =" hello"
+// // boolean 
 
-// boolean 
+// let isTrue =true;
 
-let isTrue =true;
+// // undefinde
+// let x;
+// console.log(x);
 
-// undefinde
-let x;
-console.log(x);
+// let y = null;
 
-let y = null;
+// console.log(y)
 
-console.log(y)
+
+// differecne between primtive and non primitive 
+
+// let age = 24;
+//   age = 36;
+//   console.log(age)
+
+let oddnumber = [1,3,5];
+let person ={
+    name:"amandeep",
+    age:"28",
+    grade:["A","B","C"],
+    greet:function() {
+        console.log(this.name)
+    }
+}
+
+console.log(person.greet())
 
