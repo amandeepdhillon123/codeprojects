92f8d378bb4f9c2b6772157ea65269bd56616b96
amandeepdhillon123 <amandeep24397@gmail.com>
2023-11-04 10:40:33 +0530

    assignment operator

--- amariya2/javascript/code/2.var and Database/operator.js
+++ amariya2/javascript/code/2.var and Database/operator.js
@@ -1,13 +1,19 @@
 
 // arithmetic operator 
-let x = 5;
-let y = 4;
+// let x = 5;
+// let y = 4;
 
-console.log( x+ y);
-console.log( x- y);
-console.log( x * y);
-console.log( x / y);
-console.log( x % y);
+// console.log( x+ y);
+// console.log( x- y);
+// console.log( x * y);
+// console.log( x / y);
+// console.log( x % y);
 
 
-console.log( x ** y);
+// console.log( x ** y);
+
+// assignment operator 
+let x= 10;
+x = x + 5;
+x *=5
+console.log(x)
