3775500188204eb1f7ea142149bedb0b544da0d8
amandeepdhillon123 <amandeep24397@gmail.com>
2023-11-04 10:24:07 +0530

    typesof operator

--- amariya2/javascript/code/2.var and Database/data.js
+++ amariya2/javascript/code/2.var and Database/data.js
@@ -20,15 +20,33 @@
 //   age = 36;
 //   console.log(age)
 
-let oddnumber = [1,3,5];
-let person ={
-    name:"amandeep",
-    age:"28",
-    grade:["A","B","C"],
-    greet:function() {
-        console.log(this.name)
-    }
-}
+// let oddnumber = [1,3,5];
+// let person ={
+//     name:"amandeep",
+//     age:"28",
+//     grade:["A","B","C"],
+//     greet:function() {
+//         console.log(this.name)
+//     }
+// }
 
-console.log(person.greet())
+// console.log(person.greet())
+
+
+let num= 20;
+let str = "hell stirng";
+let bool = true;
+let obj = { key :"value"};
+let arr =[1,2,3];
+let fun =function(){};
+
+console.log(typeof(num))
+console.log(typeof(str))
+console.log(typeof(bool))
+console.log(typeof(obj))
+console.log(typeof(arr))
+console.log(typeof(fun))
+
+
+
 
