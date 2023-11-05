902a422aeb453e07efc3d9ebd48a03b6684d0bed
amandeepdhillon123 <amandeep24397@gmail.com>
2023-11-05 08:42:31 +0530

    admin login logout

--- amariya/Fashion/server/middleware/admin/adminAuth.js
+++ amariya/Fashion/server/middleware/admin/adminAuth.js
@@ -2,13 +2,16 @@ require("dotenv").config();
 const adminDB = require("../../model/admin/adminModel");
 const jwt = require("jsonwebtoken");
 
-const adminauthenticate = async(req,res,next)=>{
+exports.adminauthenticate = async(req,res,next)=>{
     try {
         const token = req.headers.authorization;
-        
-        const verifyToken = jwt.verify(token,SECRET_KEY);
+        //   console.log(token)
+
+        const verifyToken = jwt.verify(token,process.env.JWT_SECRET);
+        // console.log(verifyToken)
         
         const rootUser = await adminDB.findOne({_id:verifyToken._id});
+        // console.log(rootUser)
         
         if(!rootUser){throw new Error("user not found")}
 
@@ -23,5 +26,3 @@ const adminauthenticate = async(req,res,next)=>{
     }
 }
 
-module.exports = adminauthenticate;
- 
--- amariya/Fashion/server/routes/admin/adminAuthRoutes.js
+++ amariya/Fashion/server/routes/admin/adminAuthRoutes.js
@@ -3,9 +3,15 @@
 const express = require("express");
 const router =express.Router();
 const adminUpload =require("../../multerConfig/admin/adminStorage")
-const {Register ,Login}= require("../../controllers/admin/adminController")
+const {Register ,Login, AdminVerify, Logout}= require("../../controllers/admin/adminController")
+const {adminauthenticate}= require("../../middleware/admin/adminAuth")
 
 router.post("/register",adminUpload.single("admin_profile"),Register)
 router.post("/login",Login)
+router.get("/logout",adminauthenticate,Logout)
+// admin verify 
+router.get("/adminverify",adminauthenticate,AdminVerify);
+
+
 
 module.exports = router
