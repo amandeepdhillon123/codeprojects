142ac86a10d85b15701af8fa12bcc8ae37ed043e
amandeepdhillon123 <amandeep24397@gmail.com>
2023-11-12 20:45:59 +0530

    cart features

--- amariya1/fullSatck_Projects/1.fashion/server/controllers/cart/cartController.js
+++ amariya1/fullSatck_Projects/1.fashion/server/controllers/cart/cartController.js
@@ -126,3 +126,46 @@ exports.removeSingleiteam = async (req, res) => {
 
     }
 }
+
+
+// ---------------->>>>>> remove all item controller <<<<<-------------
+  
+exports.removeAlliteam = async(req,res)=>{
+    const { id } = req.params;
+    try {
+        
+        const productfind = await productsdb.findOne({ _id: id });
+
+        const carts = await cartsdb.findOne({ userid: req.userId, productid: productfind._id });
+
+        if (!carts) {
+            res.status(400).json({ error: "cart item not found" });
+        }
+
+        const deleteCartItem = await cartsdb.findByIdAndDelete({_id:carts._id});
+
+
+        // prodcut increment
+        productfind.quantity = productfind.quantity + carts.quantity 
+        await productfind.save();
+
+
+        res.status(200).json({message:"YOur Iteam sucessfully remove in your cart",deleteCartItem});
+    } catch (error) {
+        res.status(400).json(error)
+        
+    }
+}
+
+
+// ------------>>>> remove cart data <<<<<<<-------------
+
+// DeleteCartsData
+exports.DeleteCartsData = async(req,res)=>{
+    try {
+        const DeleteCarts = await cartsdb.deleteMany({userid:req.userId});
+        res.status(200).json(DeleteCarts);
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
--- amariya1/fullSatck_Projects/1.fashion/server/routes/cart/cartsRoutes.js
+++ amariya1/fullSatck_Projects/1.fashion/server/routes/cart/cartsRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userauthenticate = require("../../middlewares/user/userAuthenticate")
 // import controller 
-const {AddtoCart , getCartValue ,removeSingleiteam} = require("../../controllers/cart/cartController");
+const {AddtoCart , getCartValue ,removeSingleiteam, removeAlliteam, DeleteCartsData} = require("../../controllers/cart/cartController");
 
 
 
@@ -10,5 +10,9 @@ const {AddtoCart , getCartValue ,removeSingleiteam} = require("../../controllers
 router.post("/addtocart/:id",userauthenticate,AddtoCart)
 router.get("/getcarts",userauthenticate,getCartValue)
 router.delete("/removesingleitem/:id",userauthenticate, removeSingleiteam)
+router.delete("/removeiteams/:id",userauthenticate,removeAlliteam)
 
+
+// delete cart data when order done
+router.delete("/removecartdata",userauthenticate,DeleteCartsData)
 module.exports = router ;
