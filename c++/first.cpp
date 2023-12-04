523a08f0e6f8523b341b266b812c21d884d44914
amandeepdhillon123 <amandeep24397@gmail.com>
2023-12-04 06:29:20 +0530

    ifelse for loop

---
+++ c++/first.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // std::cout<<"hello amandeep";
+    cout<<"hello ram"<<endl;
+    cout<<" hello world \n";
+    cout<<"hello krishna";
+}
