fb185a3546888a1bcafb8e9ba1810b4fc04989ca
amandeepdhillon123 <amandeep24397@gmail.com>
2023-12-11 07:55:27 +0530

    typescript

---
+++ amariya/Typescript/pro1.ts
@@ -0,0 +1,11 @@
+// @ts-ignore
+// function sum(a:number,b:number):number{
+//     return a+b
+// }
+
+// console.log(sum(4,5))
+
+const sum =(a:number,b:number):number =>{
+   return a+b
+}
+console.log(sum(4,5))
